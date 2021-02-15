3f2638b18ce2a95832f6483661aa781ac89c8f44
yeon <jryoun0404@gmail.com>
2021-02-16 01:14:14 +0900

    Feat: Int Extension하여 UTC형태를 string으로 변환할 수 있도록 구현

---
+++ CloudNotes/CloudNotes/Extension/IntExtension.swift
@@ -0,0 +1,25 @@
+//
+//  IntExtension.swift
+//  CloudNotes
+//
+//  Created by Yeon on 2021/02/16.
+//
+
+import Foundation
+
+extension Int {
+    struct Formatter {
+        static let utcFormatter: DateFormatter = {
+            let dateFormatter = DateFormatter()
+            dateFormatter.timeZone = TimeZone.autoupdatingCurrent
+            dateFormatter.locale = NSLocale.current
+            dateFormatter.dateFormat = "yyyy.MM.dd"
+            
+            return dateFormatter
+        }()
+    }
+    
+    var stringFromUTC: String? {
+        return Formatter.utcFormatter.string(from: Date(timeIntervalSince1970: Double(self)))
+    }
+}

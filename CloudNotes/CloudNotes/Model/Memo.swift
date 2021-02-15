cb9e5547b41ff5b3f544c69c374fb8b7b65a6039
odong-Tree <ss12357@naver.com>
2021-02-15 21:26:56 +0900

    feat: Memo Model 구조체 구현

--- CloudNotes/CloudNotes/Model/Memo.swift
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -1,1 +1,16 @@
 import Foundation
+
+struct Memo: Codable {
+    var title: String
+    var body: String
+    var lastModified: Int
+    var lastModifiedDate: Date {
+        let date = Date(timeIntervalSince1970: TimeInterval(lastModified))
+        return date
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
+}
--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -18,15 +18,7 @@ class CloudNotesTests: XCTestCase {
     }
 
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        
     }
 
 }

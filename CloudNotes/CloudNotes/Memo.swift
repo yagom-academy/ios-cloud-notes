dea31bcc2fd68a8fbcae5f96994b960ddc292ec6
SM <hcooch2ch3@gmail.com>
2021-02-15 18:54:17 +0900

    feat: JSON파일 추가 및 Memo.swift, MemoModel.swift 파일 추가

---
+++ CloudNotes/CloudNotes/Memo.swift
@@ -0,0 +1,8 @@
+//
+//  Memo.swift
+//  CloudNotes
+//
+//  Created by 임성민 on 2021/02/15.
+//
+
+import Foundation
---
+++ CloudNotes/CloudNotes/MemoModel.swift
@@ -0,0 +1,8 @@
+//
+//  MemoModel.swift
+//  CloudNotes
+//
+//  Created by 임성민 on 2021/02/15.
+//
+
+import Foundation

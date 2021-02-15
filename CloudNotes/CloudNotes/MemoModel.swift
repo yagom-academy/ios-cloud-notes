e198be50545e9dcf570bc4e82454517cfae4f934
SM <hcooch2ch3@gmail.com>
2021-02-15 18:56:37 +0900

    feat: MemoModel struct 구현

--- CloudNotes/CloudNotes/MemoModel.swift
+++ CloudNotes/CloudNotes/MemoModel.swift
@@ -5,4 +5,14 @@
 //  Created by 임성민 on 2021/02/15.
 //
 
-import Foundation
+import UIKit
+
+struct MemoModel {
+    static func getData() -> [Memo]? {
+        let fileName = "sample"
+        let file = NSDataAsset(name: fileName)
+        let Memos = try? JSONDecoder().decode([Memo].self, from: file!.data)
+        dump(Memos)
+        return Memos
+    }
+}

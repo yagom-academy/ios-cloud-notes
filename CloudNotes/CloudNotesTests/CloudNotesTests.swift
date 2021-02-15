4aee63469abc31622c7ed78a53586acff0cfafe7
odong-Tree <ss12357@naver.com>
2021-02-15 21:56:56 +0900

    test: Memo 타입에 대한 Decoding 테스트 코드 작성

--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -17,8 +17,16 @@ class CloudNotesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
+    func testDecodeMemoSample() throws {
+        let data = NSDataAsset(name: "sample")!
+        let memoList: [Memo] = try JSONDecoder().decode([Memo].self, from: data.data)
         
+        XCTAssertEqual(memoList[0].title, "똘기떵이호치새초미자축인묘")
+        XCTAssertEqual(memoList[2].lastModified, 1608651333)
+        XCTAssertEqual(memoList[3].title, "네번째")
+        
+        // 실패할 테스트
+//        XCTAssertEqual(memoList[3].lastModified, 202020, "It would fail")
     }
 
 }

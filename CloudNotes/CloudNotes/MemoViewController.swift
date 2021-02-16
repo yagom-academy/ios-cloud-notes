c13f19ff85b5eecab2cea6bc57d1029f72cbe244
SM <hcooch2ch3@gmail.com>
2021-02-16 16:18:44 +0900

    feat: MemoViewController 구현(메모를 보여줄 뷰컨트롤러)

--- CloudNotes/CloudNotes/MemoViewController.swift
+++ CloudNotes/CloudNotes/MemoViewController.swift
@@ -13,9 +13,31 @@ protocol MemoViewControllerDelegate {
 }
 
 class MemoViewController: UIViewController {
+    private let memoTextView: UITextView = {
+        let textView = UITextView()
+        textView.dataDetectorTypes = .all
+        textView.textContainerInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        view.addSubview(memoTextView)
+        configureConstraints()
     }
+}
+
+extension MemoViewController {
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            memoTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            memoTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            memoTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            memoTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
 
 }

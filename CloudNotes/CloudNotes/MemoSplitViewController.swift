400c094718ae80991c6bfd0f319cab7a98fa9e75
SM <hcooch2ch3@gmail.com>
2021-02-16 11:40:19 +0900

    feat: MemoSplitViewController 구현

--- CloudNotes/CloudNotes/MemoSplitViewController.swift
+++ CloudNotes/CloudNotes/MemoSplitViewController.swift
@@ -11,19 +11,11 @@ class MemoSplitViewController: UISplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        let masterViewController = UINavigationController(rootViewController: MemoTableViewController())
+        let detailViewController = MemoViewController()
+        self.viewControllers = [masterViewController, detailViewController]
+        self.preferredPrimaryColumnWidthFraction = 1/3
+        self.preferredDisplayMode = .oneBesideSecondary
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

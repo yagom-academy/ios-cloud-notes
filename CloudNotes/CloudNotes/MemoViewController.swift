67403b4ebfc27f59748cbfe1ec4408c4bab35025
SM <hcooch2ch3@gmail.com>
2021-02-16 21:13:05 +0900

    feat: 메모에서 키보드 관련 편의기능 추가(키보드 올라오면 스크롤 아래까지 가능하는 기능, 위로 스크롤올리면 키보드 사라지는 기능)

--- CloudNotes/CloudNotes/MemoViewController.swift
+++ CloudNotes/CloudNotes/MemoViewController.swift
@@ -24,7 +24,9 @@ class MemoViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(memoTextView)
+        memoTextView.delegate = self
         configureConstraints()
+        registerKeyboardNotifications()
     }
 }
 
@@ -37,11 +39,32 @@ extension MemoViewController {
             memoTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
+    
+    private func registerKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(changeTextViewBottomInsetToKeyboardHeight), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(resetTextViewBottomInset), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func changeTextViewBottomInsetToKeyboardHeight(_ notification: Notification) {
+        guard let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else { return }
+        memoTextView.textContainerInset = UIEdgeInsets(top: 15, left: 15, bottom: keyboardFrame.height, right: 15)
+    }
+    
+    @objc func resetTextViewBottomInset(_ notification: Notification) {
+        memoTextView.textContainerInset = UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+    }
 }
 
 extension MemoViewController: MemoViewControllerDelegate {
     func setMemo(_ memo: String) {
         memoTextView.text = memo
     }
+}
+
+extension MemoViewController: UITextViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if(scrollView.panGestureRecognizer.translation(in: scrollView.superview).y > 0) {
+            view.endEditing(true)
+        }
     }
 }

b39e52496aaedba7b275e679b06e3164e34959d2
HakJu <gkrwnrh@gmail.com>
2021-02-16 16:17:38 +0900

    feat: ListCell 타입 생성

---
+++ CloudNotes/CloudNotes/ListCell.swift
@@ -0,0 +1,38 @@
+//
+//  ListCell.swift
+//  CloudNotes
+//
+//  Created by 이학주 on 2021/02/16.
+//
+
+import UIKit
+
+class ListCell: UITableViewCell {
+    
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.text = "testTitle"
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        return titleLabel
+    }()
+    private let dateLabel: UILabel = {
+        let dateLabel = UILabel()
+        dateLabel.text = "testData"
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        return dateLabel
+    }()
+    private let contentsLabel: UILabel = {
+        let contentsLabel = UILabel()
+        contentsLabel.text = "testContents"
+        contentsLabel.translatesAutoresizingMaskIntoConstraints = false
+        return contentsLabel
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}

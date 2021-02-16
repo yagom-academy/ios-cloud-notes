fb91fe97e0e45436afc4abefb058a85e0472d5d7
innieminnie <rkddlsl1001@gmail.com>
2021-02-16 15:54:43 +0900

    [#2] feat: dateLabel의 unix stamp 지역 시간 표기 구현

--- CloudNotes/CloudNotes/ViewController.swift
+++ CloudNotes/CloudNotes/ViewController.swift
@@ -55,12 +55,19 @@ extension ViewController: UITableViewDataSource {
         }
         cell.titleLabel.text = "똘기떵이호치새초미자축인묘"
         cell.predescriptionLabel.text = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.\n\nI am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.\n\nWhen, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees, and but a few stray gleams steal into the inner sanctuary, I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks, and grow familiar with the countless indescribable forms of the insects and flies, then I feel the presence of the Almighty, who formed us in his own image, and the breath"
-        cell.dateLabel.text = "1608651333"
+        cell.dateLabel.text = 1608651333.convertToDate()
         return cell
     }
-    
-    
 }
 extension ViewController: UITableViewDelegate {
     
+}
+extension Double {
+    func convertToDate() -> String {
+        let date = Date(timeIntervalSince1970: self)
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "YYYY.MM.dd."
+        dateFormatter.timeZone = .current
+        return dateFormatter.string(from: date)
+    }
 }

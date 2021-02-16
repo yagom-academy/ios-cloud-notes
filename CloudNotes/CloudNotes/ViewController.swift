905d9b35908291cd293449dbb880f5b29ac87a8c
innieminnie <rkddlsl1001@gmail.com>
2021-02-16 10:43:22 +0900

    [#1] feat: 첫 화면 네비게이션바 아이템 구현

--- CloudNotes/CloudNotes/ViewController.swift
+++ CloudNotes/CloudNotes/ViewController.swift
@@ -8,10 +8,15 @@ import UIKit
 
 class ViewController: UIViewController {
     let tableView = UITableView()
-    
+    lazy var addMemoButton: UIBarButtonItem = {
+        let button =  UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped(_:)))
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpTableView()
+        setUpNavigationBar()
     }
     
     private func setUpTableView() {
@@ -28,6 +33,15 @@ class ViewController: UIViewController {
             self.tableView.topAnchor.constraint(equalTo: safeLayoutGuide.topAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: safeLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    private func setUpNavigationBar() {
+        self.navigationItem.title = "메모"
+        self.navigationItem.rightBarButtonItem = addMemoButton
+    }
+    
+    @objc private func addButtonTapped(_ sender: Any) {
+        print("button pressed")
     }
 }
 extension ViewController: UITableViewDataSource {

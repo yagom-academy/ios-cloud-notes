9211c337dbdb0eef401949238fc2c0e30fca08bb
SM <hcooch2ch3@gmail.com>
2021-02-16 11:37:19 +0900

    feat: 스토리보드 삭제, ViewController.swift 삭제, MemoSplitViewController, MemoTableViewController, MemoViewController 파일 추가

---
+++ CloudNotes/CloudNotes/MemoSplitViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoSplitViewController.swift
+//  CloudNotes
+//
+//  Created by 임성민 on 2021/02/16.
+//
+
+import UIKit
+
+class MemoSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

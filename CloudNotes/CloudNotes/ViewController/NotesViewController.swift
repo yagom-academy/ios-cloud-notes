ddaa39d40ea86921ef37509c5c685833508d8314
zziro95 <opers7878@gmail.com>
2021-02-16 11:54:14 +0900

    Refactor #1 - sample.dataset을 활용하여 테이블뷰 표시

--- CloudNotes/CloudNotes/ViewController/NotesViewController.swift
+++ CloudNotes/CloudNotes/ViewController/NotesViewController.swift
@@ -13,6 +13,7 @@ class NotesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureTableView()
+        decodeJSONFile()
     }
     
     private func configureTableView() {
@@ -60,16 +61,16 @@ class NotesViewController: UIViewController {
 // MARK: - TableView DataSource
 extension NotesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 7
+        return sampleData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: NotesTableViewCell.identifier, for: indexPath) as? NotesTableViewCell else {
             return UITableViewCell()
         }
-        cell.titleLabel.text = "호롤ㄹ로롤롤로로로롤롤로롤롤ㄹ롤롤 나방이 홓ㄹ로롤롤ㄹ로로로ㅗ롤로롤"
-        cell.lastModifiedDateLabel.text = "1608651333"
-        cell.bodyLabel.text = "A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart. I am alone, and feel the charm of existence in this spot, which was created for the bliss of souls like mine.\n\nI am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence, that I neglect my talents. I should be incapable of drawing a single stroke at the present moment; and yet I feel that I never was a greater artist than now.\n\nWhen, while the lovely valley teems with vapour around me, and the meridian sun strikes the upper surface of the impenetrable foliage of my trees,"
+        cell.titleLabel.text = sampleData[indexPath.row].title
+        cell.lastModifiedDateLabel.text = "\(sampleData[indexPath.row].lastModifiedDate)"
+        cell.bodyLabel.text = sampleData[indexPath.row].body
         return cell
     }
 }

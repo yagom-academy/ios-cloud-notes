6e8424dec33ed33c2fd07783444b8ff72f043584
innieminnie <rkddlsl1001@gmail.com>
2021-02-16 10:31:04 +0900

    [#1] feat: 메모 리스트 테이블뷰 생성

--- CloudNotes/CloudNotes/ViewController.swift
+++ CloudNotes/CloudNotes/ViewController.swift
@@ -7,12 +7,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpTableView()
     }
-
-
+    
+    private func setUpTableView() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.view.addSubview(tableView)
+        
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        let safeLayoutGuide = self.view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            self.tableView.leadingAnchor.constraint(equalTo: safeLayoutGuide.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: safeLayoutGuide.trailingAnchor),
+            self.tableView.topAnchor.constraint(equalTo: safeLayoutGuide.topAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: safeLayoutGuide.bottomAnchor)
+        ])
+    }
 }
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
+extension ViewController: UITableViewDelegate {
+    
+}

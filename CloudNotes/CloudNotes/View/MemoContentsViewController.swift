4a88ee9775dd34b877fa1a54df8715f10ea1bba6
odong-Tree <ss12357@naver.com>
2021-02-16 19:45:44 +0900

    feat: MemoContentsViewController 오토레이아웃 추가

--- CloudNotes/CloudNotes/View/MemoContentsViewController.swift
+++ CloudNotes/CloudNotes/View/MemoContentsViewController.swift
@@ -2,8 +2,20 @@ import UIKit
 
 class MemoContentsViewController: UIViewController {
 
+    var memoTextView = UITextView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        memoTextView.dataDetectorTypes = .all
+    }
+    
+    private func setAutoLayout() {
+        NSLayoutConstraint.activate([
+            memoTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            memoTextView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            memoTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            memoTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
     }
 }

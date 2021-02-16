b2f0ffc610bc7148a27c555824c0ab1b43506c3f
Hojoon Kim <elddy0948@hotmail.co.kr>
2021-02-16 11:52:59 +0900

    feat: MemoTableViewCell.swift 파일 생성

---
+++ CloudNotes/CloudNotes/MemoTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MemoTableViewCell.swift
+//  CloudNotes
+//
+//  Created by 김호준 on 2021/02/16.
+//
+
+import UIKit
+
+class MemoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

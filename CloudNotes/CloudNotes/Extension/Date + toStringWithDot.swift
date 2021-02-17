3bde62e6befd7666fb5807b075df2ccd3d08ba2f
odong-Tree <ss12357@naver.com>
2021-02-17 20:35:53 +0900

    feat: 받아온 Date를 지역 포맷에 맞게 변환하도록 코드 추가

--- CloudNotes/CloudNotes/Extension/Date + toStringWithDot.swift
+++ CloudNotes/CloudNotes/Extension/Date + toStringWithDot.swift
@@ -2,16 +2,12 @@ import Foundation
 
 extension Date {
     func toStringWithDot() -> String {
-//        guard let currentLocale = Locale.current.regionCode else {
-//            return "en_US"
-//        }
-        
+        let currentLocale = Locale.current.regionCode ?? "ko_KR"
         let dateFormatter = DateFormatter()
-//        dateFormatter.locale = Locale(identifier: currentLocale)
-//        dateFormatter.setLocalizedDateFormatFromTemplate("yyyy. MM. dd.")
         
-        dateFormatter.dateFormat = "yyyy. MM. dd."
-        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        dateFormatter.locale = Locale(identifier: currentLocale)
+        dateFormatter.setLocalizedDateFormatFromTemplate("yyyy. MM. dd.")
+        dateFormatter.timeZone = TimeZone.autoupdatingCurrent
         return dateFormatter.string(from: self)
     }
 }

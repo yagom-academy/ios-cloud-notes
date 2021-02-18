237f4eed8f6500f21aa48373310307cdab386acd
zziro95 <opers7878@gmail.com>
2021-02-18 19:32:40 +0900

    Feat #2 - 각 셀을 눌렀을때 보일 DetailNoteViewController 파일 생성

---
+++ CloudNotes/CloudNotes/ViewController/DetailNoteViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailNoteViewController.swift
+//  CloudNotes
+//
+//  Created by Jinho Choi on 2021/02/18.
+//
+
+import UIKit
+
+class DetailNoteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

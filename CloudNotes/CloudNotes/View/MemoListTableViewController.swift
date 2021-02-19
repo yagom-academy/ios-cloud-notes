fc159ea5cee47e636d5dd8aa33ba731d260f1bc0
GREEN <humains29@gmail.com>
2021-02-19 19:30:35 +0900

    feat: SplitView 로드 시 MemoContentsView의 기본값을 설정하는 코드 추가

--- CloudNotes/CloudNotes/View/MemoListTableViewController.swift
+++ CloudNotes/CloudNotes/View/MemoListTableViewController.swift
@@ -8,10 +8,16 @@ class MemoListTableViewController: UITableViewController {
         super.viewDidLoad()
         
         configureNavigationBar()
-        
         tableView.register(MemoListTableViewCell.self, forCellReuseIdentifier: "MemoCell")
-        
+    }
+    
+    override init(style: UITableView.Style = .plain) {
+        super.init(style: style)
         decodeJSONToMemoList(fileName: "sample")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func configureNavigationBar() {
--- CloudNotes/CloudNotes/ViewController/MemoSplitViewController.swift
+++ CloudNotes/CloudNotes/ViewController/MemoSplitViewController.swift
@@ -1,16 +1,21 @@
 import UIKit
 
 class MemoSplitViewController: UISplitViewController {
+    let memoListTableViewController = MemoListTableViewController(style: .plain)
+    let memoContentsViewController = MemoContentsViewController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .white
-        
-        let memoListTableViewController = MemoListTableViewController()
+
         let memoListNavigationController = UINavigationController(rootViewController: memoListTableViewController)
-        
+        memoContentsViewController.receiveText(memo: memoListTableViewController.memoList[0])
+
         self.viewControllers = [memoListNavigationController]
         self.preferredPrimaryColumnWidthFraction = 1/3
         self.preferredDisplayMode = .oneBesideSecondary
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        self.viewControllers.append(memoContentsViewController)
     }
 }

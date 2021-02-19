2431b06a651d49ba14c699e879e8ace564599573
zziro95 <opers7878@gmail.com>
2021-02-19 14:23:50 +0900

    Feat #4 - NoteSplitViewController 파일 생성

---
+++ CloudNotes/CloudNotes/ViewController/NoteSplitViewController.swift
@@ -0,0 +1,16 @@
+//
+//  NoteSplitViewController.swift
+//  CloudNotes
+//
+//  Created by Jinho Choi on 2021/02/19.
+//
+
+import UIKit
+
+class NoteSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+}

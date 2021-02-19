d85f8e0b216905914af91ade90c83b19173c654a
SM <hcooch2ch3@gmail.com>
2021-02-19 23:43:56 +0900

    fix: makeLocaleDateFormatter의 locale identifier 관련 수정

--- CloudNotes/CloudNotes/Utility/Extensions.swift
+++ CloudNotes/CloudNotes/Utility/Extensions.swift
@@ -2,8 +2,11 @@ import Foundation
 
 extension DateFormatter {
     func makeLocaleDateFormatter() -> DateFormatter {
-        let locale = Locale.preferredLanguages[0]
-        self.locale = Locale(identifier: locale)
+        if let identifier = Locale.current.collatorIdentifier {
+            self.locale = Locale(identifier: identifier)
+        } else {
+            self.locale = Locale(identifier: Locale.current.identifier)
+        }
         self.dateStyle = .medium
         self.timeStyle = .none
         return self

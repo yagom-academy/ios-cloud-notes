79323f49db3e563aac8e2b152714b0001b2fb6d3
yeon <jryoun0404@gmail.com>
2021-02-21 22:13:39 +0900

    Refactor: fatalError 호출되지 않도록 수정

--- CloudNotes/CloudNotes/Cell/MemoTableViewCell.swift
+++ CloudNotes/CloudNotes/Cell/MemoTableViewCell.swift
@@ -44,7 +44,7 @@ final class MemoTableViewCell: UITableViewCell {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     //MARK: SetUpMemoTableViewCell

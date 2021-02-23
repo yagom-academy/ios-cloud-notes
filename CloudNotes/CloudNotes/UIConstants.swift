d3fffb3e1933abee1157c060a496efc16bf54d62
KyungminLee <kyungmin.lee.dev@gmail.com>
2021-02-23 21:06:18 +0900

    Refactor #8 - 오토레이아웃 Constraints 상수값들을 UIConstants로 분리하여 관리

---
+++ CloudNotes/CloudNotes/UIConstants.swift
@@ -0,0 +1,24 @@
+//
+//  UIConstants.swift
+//  CloudNotes
+//
+//  Created by Kyungmin Lee on 2021/02/23.
+//
+
+import UIKit
+
+struct UIConstants {
+    struct layout {
+        // MARK: - NotesTableViewCell
+        static let notesCellTitleLabelTopOffset: CGFloat = 5
+        static let notesCellTitleLabelLeadingOffset: CGFloat = 5
+        static let notesCellTitleLabelTrailingOffset: CGFloat = -5
+        
+        static let notesCellDateLabelTopOffset: CGFloat = 3
+        static let notesCellDateLabelLeadingOffset: CGFloat = 0
+        static let notesCellDateLabelBottomOffset: CGFloat = -5
+        
+        static let notesCellBodyLabelLeadingOffset: CGFloat = 40
+        static let notesCellBodyLabelTrailingOffset: CGFloat = -5
+    }
+}

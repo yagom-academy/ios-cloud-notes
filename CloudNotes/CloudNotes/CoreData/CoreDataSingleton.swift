e589690975a9eb8cafa8826ba4cab2d0f2cec789
odong-Tree <ss12357@naver.com>
2021-02-25 11:14:10 +0900

    feat: CoreDate에 대한 Singleton 객체 구현

---
+++ CloudNotes/CloudNotes/CoreData/CoreDataSingleton.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataSingleton.swift
+//  CloudNotes
+//
+//  Created by 김동빈 on 2021/02/25.
+//
+
+import Foundation
---
+++ CloudNotes/CloudNotes/CoreDataSingleton.swift
@@ -0,0 +1,83 @@
+
+import UIKit
+import CoreData
+
+class CoreDataSingleton {
+    static let shared = CoreDataSingleton()
+    
+    private init() { }
+    
+    lazy var memoData: [NSManagedObject] = {
+        return self.fetch()
+    }()
+    
+    func fetch() -> [NSManagedObject] {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return [NSManagedObject]()
+            // 에러 핸들링 필요
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Memo")
+        
+        let result = try! managedContext.fetch(fetchRequest)
+        return result
+    }
+    
+    func save(title: String, body: String, lastModified: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+            // 에러 핸들링 필요
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        let object = NSEntityDescription.insertNewObject(forEntityName: "Memo", into: managedContext)
+        object.setValue(title, forKey: "title")
+        object.setValue(body, forKey: "body")
+        object.setValue(lastModified, forKey: "lastModified")
+        
+        do {
+            try managedContext.save()
+            self.memoData.insert(object, at: 0)
+        } catch {
+            managedContext.rollback()
+        }
+    }
+    
+    func delete(object: NSManagedObject) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+            // 에러 핸들링 필요
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        managedContext.delete(object)
+        
+        do {
+            try managedContext.save()
+        } catch {
+            managedContext.rollback()
+        }
+    }
+    
+    func update(object: NSManagedObject, title: String, body: String, lastModified: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+            // 에러 핸들링 필요
+        }
+        let managedContext =
+            appDelegate.persistentContainer.viewContext
+        
+        object.setValue(title, forKey: "title")
+        object.setValue(body, forKey: "body")
+        object.setValue(lastModified, forKey: "lastModified")
+        
+        do {
+            try managedContext.save()
+        } catch {
+            managedContext.rollback()
+        }
+    }
+}

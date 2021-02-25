f5a3abb914e94a3e89ad50c8b7f5d27af59ae52c
odong-Tree <ss12357@naver.com>
2021-02-25 20:36:47 +0900

    style: SplitVC 메서드 구분하여 코드 정리

--- CloudNotes/CloudNotes/ViewController/MemoSplitViewController.swift
+++ CloudNotes/CloudNotes/ViewController/MemoSplitViewController.swift
@@ -1,18 +1,25 @@
 import UIKit
 
 class MemoSplitViewController: UISplitViewController {
-    private let memoListTableViewController = MemoListTableViewController()
-    private let memoContentsViewController = MemoContentsViewController()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.delegate = self
+        configureViewControllers()
+        configureSplitViewController()
+    }
+    
+    private func configureViewControllers() {
+        let memoListTableViewController = MemoListTableViewController()
+        let memoContentsViewController = MemoContentsViewController()
         let memoListNavigationController = UINavigationController(rootViewController: memoListTableViewController)
         let memoContentsNavigationViewController = UINavigationController(rootViewController: memoContentsViewController)
 //        memoContentsViewController.receiveText(memo: CoreDataSingleton.shared.memoData[0])
-
+        
         self.viewControllers = [memoListNavigationController, memoContentsNavigationViewController]
+    }
+    
+    private func configureSplitViewController() {
+        self.delegate = self
         self.preferredPrimaryColumnWidthFraction = 1/3
         self.preferredDisplayMode = .oneBesideSecondary
     }

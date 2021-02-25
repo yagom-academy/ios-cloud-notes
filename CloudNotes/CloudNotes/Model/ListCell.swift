3eb2bdf17c372761cbf8e4241317eafcaef94602
HakJu <gkrwnrh@gmail.com>
2021-02-25 10:38:32 +0900

    refactor: ListCell - required init 수정

--- CloudNotes/CloudNotes/Model/ListCell.swift
+++ CloudNotes/CloudNotes/Model/ListCell.swift
@@ -36,7 +36,7 @@ class ListCell: UITableViewCell {
     }
     
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
     }
     
     func updateCell(info: Memo) {

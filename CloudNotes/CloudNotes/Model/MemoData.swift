28df31f45d9e1fc9891ffe224229ca1f71c22b90
HakJu <gkrwnrh@gmail.com>
2021-03-03 02:32:12 +0900

    feat: MemoData - create method 구현

--- CloudNotes/CloudNotes/Model/MemoData.swift
+++ CloudNotes/CloudNotes/Model/MemoData.swift
@@ -16,8 +16,23 @@ final class MemoData {
     
     func read() {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
         do {
             self.list = try context.fetch(Memo.fetchRequest())
+        }
+        catch {}
+    }
+    
+    func create(title: String?, contents: String?) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let newMemo = Memo(context: context)
+        newMemo.title = title
+        newMemo.contents = contents
+        newMemo.lastModifiedDate = Date()
+        
+        do {
+            try context.save()
+            read()
         }
         catch {}
     }

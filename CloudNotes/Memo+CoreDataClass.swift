0a426f69e854ea67af90b5b9cf417349e6bae56c
SM <hcooch2ch3@gmail.com>
2021-03-02 12:21:32 +0900

    feat: Memo 기존 모델 삭제하고 CoreData 모델 생성

---
+++ CloudNotes/Memo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Memo+CoreDataClass.swift
+//  CloudNotes
+//
+//  Created by 임성민 on 2021/02/27.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Memo)
+public class Memo: NSManagedObject {
+
+}

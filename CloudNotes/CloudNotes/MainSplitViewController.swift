b0c46b1b8674072696447d2a58c92ce48e6b6769
stevenkim18 <11882@naver.com>
2021-05-31 20:07:21 +0900

    feat: 스토리보드 없이 SplitViewController 로딩 성공

---
+++ CloudNotes/CloudNotes/MainSplitViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainSplitViewController.swift
+//  CloudNotes
+//
+//  Created by steven on 2021/05/31.
+//
+
+import UIKit
+
+class MainSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

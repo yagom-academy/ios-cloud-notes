1832278f8cdeb464e6592a61bc8f49914364381c
stevenkim18 <11882@naver.com>
2021-06-01 08:02:02 +0900

    add: ListTableViewCell 추가

---
+++ CloudNotes/CloudNotes/Cell/ListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ListTableViewCell.swift
+//  CloudNotes
+//
+//  Created by steven on 2021/06/01.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CloudNotes/CloudNotes/ViewController/TextViewController.swift
+++ CloudNotes/CloudNotes/ViewController/TextViewController.swift
@@ -14,18 +14,6 @@ class TextViewController: UIViewController {
         view.backgroundColor = .red
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: nil)
-        // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }

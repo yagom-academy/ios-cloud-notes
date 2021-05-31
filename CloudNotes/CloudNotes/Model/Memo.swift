1675b53a8706547f3d228ed15383e6b8eac22558
Wody95 <kwu0626@gmail.com>
2021-05-31 19:36:35 +0900

    feat : Memo 구조체 구현 및 테스트

---
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -0,0 +1,19 @@
+//
+//  Memo.swift
+//  CloudNotes
+//
+//  Created by 기원우 on 2021/05/31.
+//
+
+import Foundation
+
+struct Memo: Decodable {
+    let title: String
+    let body: String
+    let lastModified: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
+}
--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -9,23 +9,17 @@ import XCTest
 
 class CloudNotesTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_MemoType_Decoding() {
+        guard let data: NSDataAsset = NSDataAsset(name: "sample") else {
+            print("에셋 데이터 읽어오기 실패")
+            XCTFail()
+            return
+        }
+        
+        guard let _ = try? JSONDecoder().decode([Memo].self, from: data.data) else {
+            print("Memo 타입 디코딩 실패")
+            XCTFail()
+            return
         }
     }
 

921bd2d05d7dec25bbfad98ec615490c09517e71
stevenkim18 <11882@naver.com>
2021-06-01 21:55:41 +0900

    test: sample 데이터 파싱 테스트

--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -9,24 +9,20 @@ import XCTest
 
 class CloudNotesTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_샘플_데이터_Json_파싱() {
+        let jsonDecoder = JSONDecoder()
+        jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
+        
+        guard let jsonData = NSDataAsset(name: "sample") else {
+            XCTFail()
+            return
         }
+        
+        guard let memoList = try? jsonDecoder.decode([Memo].self, from: jsonData.data) else {
+            XCTFail()
+            return
+        }
+        
+        XCTAssertEqual(memoList.count, 15)
     }
-
 }

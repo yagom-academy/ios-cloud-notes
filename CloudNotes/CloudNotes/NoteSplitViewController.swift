432e81ffc090e434160224c8d42372c516c7f4e3
fezravien <yjaewoong@naver.com>
2021-06-01 21:35:31 +0900

    ✨ [feat] : primary에 TableView 구현
    SplitView primary TableView cell
    - title
    - date
    - description

--- CloudNotes/CloudNotes/NoteSplitViewController.swift
+++ CloudNotes/CloudNotes/NoteSplitViewController.swift
@@ -30,16 +30,100 @@ extension NoteSplitViewController: UISplitViewControllerDelegate {
     }
 }
 
-class ViewController1: UIViewController {
+class ViewController1: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private let tableView: UITableView = {
+        let tableview = UITableView()
+        return tableview
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let label = UILabel()
-        self.view.addSubview(label)
-        self.navigationItem.title = "메모"
-        label.text = "first"
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.register(NoteTableCell.self, forCellReuseIdentifier: "NoteCell")
+        setConstraint()
+        
+    }
+    
+    private func setConstraint() {
+        self.view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "NoteCell", for: indexPath) as? NoteTableCell else {
+            return UITableViewCell()
+        }
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return CGFloat(self.view.frame.height * 1/12)
+    }
+}
+
+class NoteTableCell: UITableViewCell {
+    private let titleLabel: UILabel = {
+        let title = UILabel()
+        title.text = "FezzTitle"
+        return title
+    }()
+    
+    private let dateLabel: UILabel = {
+        let title = UILabel()
+        title.text = "2021. 06. 01"
+        return title
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let title = UILabel()
+        title.text = "note note note note"
+        title.textColor = UIColor.lightGray
+        return title
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setConstraint() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(descriptionLabel)
+        
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        dateLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            dateLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            dateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            dateLabel.trailingAnchor.constraint(lessThanOrEqualTo: descriptionLabel.leadingAnchor),
+            descriptionLabel.centerYAnchor.constraint(equalTo: dateLabel.centerYAnchor),
+            descriptionLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15)
+        ])
     }
 }
 

be9c1bc569ccfd25615f24d8b96b240b11e8db30
SJ <boromir21@naver.com>
2021-06-01 17:59:08 +0900

    feat. 메인화면 뷰컨트롤러를 코드로 구현 메인 스토리보드를 삭제하고 루트뷰에 스플릿뷰를 연결하여 초기화

---
+++ CloudNotes/CloudNotes/MemoFormViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemoFormViewController.swift
+//  CloudNotes
+//
+//  Created by TORI on 2021/06/01.
+//
+
+import UIKit
+
+class MemoFormViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
---
+++ CloudNotes/CloudNotes/MemoListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MemoListViewController.swift
+//  CloudNotes
+//
+//  Created by TORI on 2021/06/01.
+//
+
+import UIKit
+
+class MemoListViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}

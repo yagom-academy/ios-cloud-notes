8ec989533b4df4915c0fb12cb5999a475059db54
stevenkim18 <11882@naver.com>
2021-06-01 20:52:57 +0900

    feat: textView 생성

--- CloudNotes/CloudNotes/ViewController/TextViewController.swift
+++ CloudNotes/CloudNotes/ViewController/TextViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class TextViewController: UIViewController {
-
+    
+    let textView: UITextView = {
+        let textView = UITextView()
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.font = UIFont.preferredFont(forTextStyle: .title1)
+        return textView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        view.addSubview(textView)
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: nil)
+        
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.topAnchor),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
 }

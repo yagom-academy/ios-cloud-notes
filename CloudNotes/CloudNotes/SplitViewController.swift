3c0f72c27a2bea932da21f0caf95822eb025c0af
eunseo5355 <eunseo5355@naver.com>
2021-06-01 17:47:04 +0900

    ✨ [Feat] 프로젝트 초기 설정
    - ViewController -> SplitViewController로 이름 변경 후 UISplitViewController로 변경
    - 스토리보드 삭제 후 SceneDelegate에서 초기 화면을 SplitViewController로 설정

---
+++ CloudNotes/CloudNotes/SplitViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CloudNotes - SplitViewController.swift
+//  Created by Hailey.
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+

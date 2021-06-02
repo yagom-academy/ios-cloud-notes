bdffd4d315a18d99b28246b0f5e7ce0442cd83cb
duckbok <duckbok22@gmail.com>
2021-06-02 19:21:02 +0900

    [Feat] #2 Decodable Memo Type 정의

---
+++ CloudNotes/CloudNotes/Sources/Models/Memo.swift
@@ -0,0 +1,21 @@
+//
+//  Memo.swift
+//  CloudNotes
+//
+//  Created by duckbok on 2021/06/02.
+//
+
+import Foundation
+
+struct Memo: Decodable {
+
+    let title: String
+    let body: String
+    let lastModified: TimeInterval
+
+    private enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
+
+}

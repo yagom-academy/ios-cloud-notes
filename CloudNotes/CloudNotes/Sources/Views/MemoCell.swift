d8849aa78706ff35e180e4aefddb2ae3325e25a1
duckbok <duckbok22@gmail.com>
2021-06-02 22:15:24 +0900

    [Feat] #3 MemoCell UI 요소 정의

--- CloudNotes/CloudNotes/Sources/Views/MemoCell.swift
+++ CloudNotes/CloudNotes/Sources/Views/MemoCell.swift
@@ -7,4 +7,45 @@
 
 import UIKit
 
-final class MemoCell: UITableViewCell { }
+final class MemoCell: UITableViewCell {
+
+    // MARK: UI
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        return label
+    }()
+
+    private let lastModifiedDateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.setContentCompressionResistancePriority(.required, for: .horizontal)
+        label.setContentHuggingPriority(.required, for: .horizontal)
+        return label
+    }()
+
+    private let oneLineBodyLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.textColor = .systemGray
+        return label
+    }()
+
+    private let footStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.alignment = .center
+        stackView.axis = .horizontal
+        stackView.spacing = 20
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+    private let contentStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+
+}

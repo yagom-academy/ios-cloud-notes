88ab2211ecaae043f6b09c300b270fa70e38b9bf
stevenkim18 <11882@naver.com>
2021-06-02 19:56:02 +0900

    chore: 코드 정리

--- CloudNotes/CloudNotes/ViewController/MainSplitViewController.swift
+++ CloudNotes/CloudNotes/ViewController/MainSplitViewController.swift
@@ -11,22 +11,19 @@ class MainSplitViewController: UISplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         let navigationEmbeddedListTableViewController = UINavigationController(rootViewController: ListTableViewController())
-        
         let navigationEmbeddedTextViewController = UINavigationController(rootViewController: TextViewController())
-        
         self.viewControllers = [navigationEmbeddedListTableViewController, navigationEmbeddedTextViewController]
-        
         self.preferredDisplayMode = .oneBesideSecondary
-        
         self.delegate = self
     }
 
 }
 
 extension MainSplitViewController: UISplitViewControllerDelegate {
+    
     func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
         return true
     }
+    
 }

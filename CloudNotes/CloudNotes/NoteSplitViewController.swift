3a902c0ed9651cd455f75e3892d33067c086a6f6
fezravien <yjaewoong@naver.com>
2021-06-03 00:02:42 +0900

    ♻️ [refactor] : fatal error 제거

--- CloudNotes/CloudNotes/NoteSplitViewController.swift
+++ CloudNotes/CloudNotes/NoteSplitViewController.swift
@@ -126,7 +126,7 @@ class NoteTableCell: UITableViewCell {
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
     
     private func setConstraint() {

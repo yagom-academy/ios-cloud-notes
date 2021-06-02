2e09a3b6d4c7430e888454ce862a3037ac0d5c7c
duckbok <duckbok22@gmail.com>
2021-06-02 20:32:32 +0900

    [Feat] #3 MemoList를 위한 TableView 표시되게 설정

--- CloudNotes/CloudNotes/Sources/ViewControllers/MemoListViewController.swift
+++ CloudNotes/CloudNotes/Sources/ViewControllers/MemoListViewController.swift
@@ -7,10 +7,40 @@
 
 import UIKit
 
-class MemoListViewController: UIViewController {
+final class MemoListViewController: UIViewController {
+
+    private let tableView = UITableView()
+    private var memos = [Memo]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
+    }
+
+    private func configureTableView() {
+        tableView.register(MemoCell.self, forCellReuseIdentifier: "memoCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+
+}
+
+extension MemoListViewController: UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        memos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "memoCell", for: indexPath)
     }
 
 }
---
+++ CloudNotes/CloudNotes/Sources/Views/MemoCell.swift
@@ -0,0 +1,10 @@
+//
+//  MemoCell.swift
+//  CloudNotes
+//
+//  Created by duckbok on 2021/06/02.
+//
+
+import UIKit
+
+final class MemoCell: UITableViewCell { }

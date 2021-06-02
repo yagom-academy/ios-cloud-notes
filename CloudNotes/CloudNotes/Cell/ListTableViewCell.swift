f36fa4b86f8ee97e49ffca2ea696eaa7549778f8
stevenkim18 <11882@naver.com>
2021-06-02 19:55:21 +0900

    style: 기능들을 함수로 나눔.

--- CloudNotes/CloudNotes/Cell/ListTableViewCell.swift
+++ CloudNotes/CloudNotes/Cell/ListTableViewCell.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class ListTableViewCell: UITableViewCell {
+    
     static let identifier = "ListTableViewCell"
     
     let titleLabel: UILabel = {
@@ -34,11 +35,21 @@ class ListTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        addSubviews()
+        setConstraintBetweenViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func addSubviews() {
         self.contentView.addSubview(titleLabel)
         self.contentView.addSubview(dateLabel)
         self.contentView.addSubview(bodyLabel)
-        
+    }
+    
+    func setConstraintBetweenViews() {
         NSLayoutConstraint.activate([
             titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 5),
             titleLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 5),
@@ -51,10 +62,6 @@ class ListTableViewCell: UITableViewCell {
             bodyLabel.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -5),
             bodyLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5)
         ])
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
 }

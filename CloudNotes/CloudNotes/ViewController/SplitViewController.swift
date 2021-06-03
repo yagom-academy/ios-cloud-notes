913b1a8bfecce9cdc02cf4cb95640ab8d9899add
kane-young <lyw2100@naver.com>
2021-06-03 18:56:54 +0900

    Feat : Storyboard 삭제 및 초기 화면 설정

---
+++ CloudNotes/CloudNotes/ViewController/SplitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainSplitViewController.swift
+//  CloudNotes
+//
+//  Created by 이영우 on 2021/06/02.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}

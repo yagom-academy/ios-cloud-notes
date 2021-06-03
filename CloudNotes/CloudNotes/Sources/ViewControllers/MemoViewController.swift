a2790991dd6623b41405787da9769ad6ffbe841f
duckbok <duckbok22@gmail.com>
2021-06-03 16:19:16 +0900

    [Feat] #4 Memo 화면 UI 요소 정의

--- CloudNotes/CloudNotes/Sources/ViewControllers/MemoViewController.swift
+++ CloudNotes/CloudNotes/Sources/ViewControllers/MemoViewController.swift
@@ -7,10 +7,36 @@
 
 import UIKit
 
-class MemoViewController: UIViewController {
+final class MemoViewController: UIViewController {
+
+    // MARK: UI
+
+    private let textView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.isEditable = true
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+
+    // MARK: View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextView()
+    }
+
+    // MARK: Configure
+
+    private func configureTextView() {
+        view.addSubview(textView)
+
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: view.topAnchor),
+            textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 
 }

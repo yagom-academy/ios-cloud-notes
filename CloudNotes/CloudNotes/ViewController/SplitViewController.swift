82b5e700a05dd88d9784f740fdddc993c66282c3
kane-young <lyw2100@naver.com>
2021-06-03 20:58:56 +0900

    Feat : SplitViewController에 MemoListViewController, MemoDetailViewController 삽입

--- CloudNotes/CloudNotes/ViewController/SplitViewController.swift
+++ CloudNotes/CloudNotes/ViewController/SplitViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 class SplitViewController: UISplitViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    self.delegate = self
+    self.viewControllers = [
+      UINavigationController(rootViewController: MemoListViewController()),
+      UINavigationController(rootViewController: MemoDetailViewController())
+    ]
+    self.preferredDisplayMode = .oneBesideSecondary
+  }
+}
+
+extension SplitViewController: UISplitViewControllerDelegate {
+  func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+    return true
   }
 }

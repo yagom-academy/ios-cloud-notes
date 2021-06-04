eecc95e530a19e0b173fd2867b6d7e155975fe69
eunseo5355 <eunseo5355@naver.com>
2021-06-04 18:10:29 +0900

    ✨ [Feat] Model 타입에서 날짜 포맷해주는 프로퍼티 추가
    - 구조체명 NoteItem -> NoteData로 수정

---
+++ CloudNotes/CloudNotes/NoteData.swift
@@ -0,0 +1,29 @@
+//
+//  NoteItem.swift
+//  CloudNotes
+//
+//  Created by 배은서 on 2021/06/01.
+//
+
+import Foundation
+
+struct NoteData: Decodable {
+    let title: String
+    let body: String
+    let lastModified: Int
+    var formattedLastModified: String {
+        let date = Date(timeIntervalSince1970: TimeInterval(lastModified))
+        let dateFormatter = DateFormatter()
+        let locale = NSLocale.autoupdatingCurrent
+        dateFormatter.dateFormat = "yyyy.MM.dd."
+        dateFormatter.locale = locale
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    private enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
+    
+}

0598c569180a7a31957874b9fbb27774cdd4dac8
eunseo5355 <eunseo5355@naver.com>
2021-06-05 21:43:23 +0900

    ✨ [Feat] Split View 구현
    - 가로가 compact size인 화면에서는 메모 리스트가 뜨도록 구현

--- CloudNotes/CloudNotes/SplitViewController.swift
+++ CloudNotes/CloudNotes/SplitViewController.swift
@@ -6,12 +6,30 @@
 
 import UIKit
 
-class SplitViewController: UISplitViewController {
-
+final class SplitViewController: UISplitViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.delegate = self
+        self.preferredDisplayMode = .oneBesideSecondary
+        NoteListViewController.noteDelegate = self
+        
+        self.viewControllers = [
+            UINavigationController(rootViewController: NoteListViewController()),
+            UINavigationController(rootViewController: NoteDetailViewController())
+        ]
     }
-
-
 }
 
+extension SplitViewController: NoteDelegate {
+    func showDetailNote(data: NoteData) {
+        let noteDetailNC = UINavigationController(rootViewController: NoteDetailViewController())
+        (noteDetailNC.viewControllers.last as? NoteDetailViewController)?.configureTextView(data)
+        self.showDetailViewController(noteDetailNC, sender: self)
+    }
+}
+
+extension SplitViewController: UISplitViewControllerDelegate {
+    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
+        return true
+      }
+}

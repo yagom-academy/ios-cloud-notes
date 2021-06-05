f2a16c205c6e16e5b76bf7bc6c6b07743921ccb2
eunseo5355 <eunseo5355@naver.com>
2021-06-05 21:24:57 +0900

    ✨ [Feat] NoteDelegate 프로토콜 구현

---
+++ CloudNotes/CloudNotes/NoteDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  NoteDelegate.swift
+//  CloudNotes
+//
+//  Created by 배은서 on 2021/06/03.
+//
+
+import Foundation
+
+protocol NoteDelegate: AnyObject {
+    func showDetailNote(data: NoteData)
+}

1b491dc580d1e21a6b7be0c007314c0ac174b27a
cjm <stepsw112@gmail.com>
2021-06-07 20:49:11 +0900

    Refactor : blank를 AllItem으로 변경

--- CloudNotes/CloudNotes/CoreDataEntities/MemoListItem+CoreDataProperties.swift
+++ CloudNotes/CloudNotes/CoreDataEntities/MemoListItem+CoreDataProperties.swift
@@ -19,8 +19,7 @@ extension MemoListItem {
     @NSManaged public var body: String?
     @NSManaged public var lastModifiedDate: Date?
     @NSManaged public var title: String?
-    @NSManaged public var blankOverTitle: String?
-    @NSManaged public var blankBetweenTitleAndBody: String?
+    @NSManaged public var allText: String?
 
 }
 

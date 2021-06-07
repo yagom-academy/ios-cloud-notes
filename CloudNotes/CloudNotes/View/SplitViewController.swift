22e98c2245c8317276e2e3fed9dc7cfabe89dec2
kangkyung <rudgns0508@naver.com>
2021-06-07 18:59:41 +0900

    Refator: 코드이동(SceneDelegate -> SplitViewController)
    - SceneDelegate에서는 화면에 무엇을 보여줄지 가장 근간이 되는 코드만 남도록
      rootViewController를 지정해주는 작업만 하고
      나머지 작업은 SplitViewController 내부에서 처리하도록 변경

--- CloudNotes/CloudNotes/View/SplitViewController.swift
+++ CloudNotes/CloudNotes/View/SplitViewController.swift
@@ -8,14 +8,41 @@
 import UIKit
 
 class SplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+  private let listVC = ListViewController()
+  private let memoVC = MemoViewController()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.delegate = self
+    setViewControllers()
+  }
+  
+  private func setViewControllers() {
+    delegate = self
+    
+    listVC.title = "메모"
+    listVC.delegate = self
+    
+    viewControllers = [
+      UINavigationController(rootViewController: listVC),
+      UINavigationController(rootViewController: memoVC)
+    ]
   }
   
   func splitViewController(_ splitViewController: UISplitViewController,
                            collapseSecondary secondaryViewController: UIViewController,
                            onto primaryViewController: UIViewController) -> Bool {
     return true
+  }
+}
+
+extension SplitViewController: ListViewControllerDelegate {
+  func didTapMenuItem(at index: Int, model memoInfo: MemoInfo) {
+    memoVC.viewModel.update(model: memoInfo)
+    memoVC.updateUI()
+    
+    // FIXME: - 기기가 아이폰일 경우, 화면전환이 안되는 문제
+    if UIDevice.current.userInterfaceIdiom == .phone {
+      listVC.navigationController?.pushViewController(memoVC, animated: true)
+    }
   }
 }

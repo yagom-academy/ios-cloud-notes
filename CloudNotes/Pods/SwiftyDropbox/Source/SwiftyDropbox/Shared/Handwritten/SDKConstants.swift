277c8b871898716aab79d5c37e84cb238bfc03ee
cjm <stepsw112@gmail.com>
2021-06-08 18:08:53 +0900

    Feat: cocoapods이란 의존성 도구를 이용하여 'SwiftyDropbox'를 설치함

---
+++ CloudNotes/Pods/SwiftyDropbox/Source/SwiftyDropbox/Shared/Handwritten/SDKConstants.swift
@@ -0,0 +1,10 @@
+///
+/// Copyright (c) 2016 Dropbox, Inc. All rights reserved.
+///
+
+import Foundation
+
+struct Constants {
+    static let versionSDK = "7.0.1"
+    static let kCSRFKey = "kCSRFKeySwiftSDK"
+}

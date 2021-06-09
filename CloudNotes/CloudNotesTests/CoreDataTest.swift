0efacdaf2cee1f0aeb88646d140ad4113c88e161
stevenkim18 <11882@naver.com>
2021-06-09 17:15:03 +0900

    test: Memo 모델로 코어데이터 저장 테스트 성공

--- CloudNotes/CloudNotesTests/CoreDataTest.swift
+++ CloudNotes/CloudNotesTests/CoreDataTest.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import CoreData
 @testable import CloudNotes
 
 class CoreDataTest: XCTestCase {
@@ -15,9 +16,25 @@ class CoreDataTest: XCTestCase {
         // context를 가져 온다. -> CoreDataManager 안에 있음.
         
         // Entity를 가져온다.
-//        guard let entity = CoreDataManager else {
-//            <#statements#>
-//        }
+        guard let entity = CoreDataManager.shared.entity else {
+            XCTFail()
+            return
+        }
+        
+        let memo = NSManagedObject(entity: entity, insertInto: CoreDataManager.shared.mainContext)
+        memo.setValue("제목", forKey: "title")
+        memo.setValue("내용내용내용", forKey: "body")
+//        memo.setValue(NSData(), forKey: "lastModified")
+        
+        CoreDataManager.shared.saveContext()
+       
+        do {
+            let memos = try CoreDataManager.shared.mainContext.fetch(Memo.fetchRequest()) as! [Memo]
+            XCTAssertNil(memos.count)
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+        
         
     }
 }

77c0c122b39c9a5d1696b456e4ba9e05ea1a1fb2
fezravien <yjaewoong@naver.com>
2021-06-09 21:28:44 +0900

    ✨ [feat] : 코어 데이터 모델 생성 및 기능 구현
    fetch
    insert
    delete

---
+++ CloudNotes/CloudNotes/Model/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  CloudNotes
+//
+//  Created by 윤재웅 on 2021/06/09.
+//
+
+import Foundation
+
+struct Note {
+    let title: String?
+    let body: String?
+    let lastModify: Date?
+}
--- CloudNotes/CloudNotes/Model/NoteManager.swift
+++ CloudNotes/CloudNotes/Model/NoteManager.swift
@@ -5,30 +5,57 @@
 //  Created by 윤재웅 on 2021/06/03.
 //
 
-import Foundation
+import CoreData
 import UIKit
 
-struct NoteManager {
-    private var noteDatas: [Note]?
+final class NoteManager {
+    private lazy var context: NSManagedObjectContext = {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        return appDelegate.persistentContainer.viewContext
+    }()
     
-    init() {
-        guard let jsonData = NSDataAsset(name: "sample") else { return }
-        guard let data = try? JSONDecoder().decode([Note].self, from: jsonData.data) else { return }
-        noteDatas = data
-    }
-    
-    var dataCount: Int {
-        return noteDatas?.count ?? 0
-    }
-    
-    func dataAtIndex(_ index: Int) -> Note {
-        var data = noteDatas?[index] ?? Note(title: "", description: "", lastModify: 0, date: nil)
-        let dateFormatter = DateFormatter()
-        dateFormatter.locale = Locale(identifier: Locale.current.identifier)
-        dateFormatter.setLocalizedDateFormatFromTemplate("yyyy.MM.dd")
-        let date = Date(timeIntervalSince1970: TimeInterval(data.lastModify))
-        data.date = dateFormatter.string(from: date)
+    func fetch() -> [Note] {
+        var noteList: [Note]?
+        let fetchRequest: NSFetchRequest<NoteModel> = NoteModel.fetchRequest()
+        let lastModifyDesc = NSSortDescriptor(key: "lastModify", ascending: false)
+        fetchRequest.sortDescriptors = [lastModifyDesc]
         
-        return data
+        do {
+            let result = try self.context.fetch(fetchRequest)
+            
+            for record in result {
+                let data = Note(title: record.title, body: record.body, lastModify: record.lastModify)
+                noteList?.append(data)
+            }
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+        return noteList ?? []
+    }
+    
+    func insert(_ data: Note) {
+        guard let object = NSEntityDescription.insertNewObject(forEntityName: "CloudNotes", into: self.context) as? NoteModel else { return }
+        object.title = data.title
+        object.body = data.body
+        object.lastModify = data.lastModify
+        
+        do {
+            try self.context.save()
+        } catch let error as NSError {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func delete(_ objectID: NSManagedObjectID) -> Bool {
+        let object = self.context.object(with: objectID)
+        self.context.delete(object)
+        
+        do {
+            try self.context.save()
+            return true
+        } catch let error as NSError {
+            print(error.localizedDescription)
+            return false
+        }
     }
 }

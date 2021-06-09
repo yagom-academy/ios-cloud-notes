807ae786a1e8d6fd6bdb8abf731a27612e8e972a
duckbok <duckbok22@gmail.com>
2021-06-09 18:42:09 +0900

    [Fix] #3 cell init?(coder:) fatalError 제거

--- CloudNotes/CloudNotes/Sources/Views/MemoCell.swift
+++ CloudNotes/CloudNotes/Sources/Views/MemoCell.swift
@@ -58,7 +58,7 @@ final class MemoCell: UITableViewCell {
     }
 
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
     }
 
     // MARK: Prepare

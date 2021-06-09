637b066cb86341c7a507edb1dfbbe6dc49dc23f4
stevenkim18 <11882@naver.com>
2021-06-09 15:26:20 +0900

    add: CoreDataManager 클래스 생성

---
+++ CloudNotes/CloudNotes/CoreData/CoreDataManager.swift
@@ -0,0 +1,46 @@
+//
+//  CoreDataManager.swift
+//  CloudNotes
+//
+//  Created by steven on 2021/06/09.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    private init() {
+        
+    }
+    
+    var mainContext: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    // MARK: - Core Data stack
+    lazy var persistentContainer: NSPersistentCloudKitContainer = {
+        let container = NSPersistentCloudKitContainer(name: "CloudNotes")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+    
+}

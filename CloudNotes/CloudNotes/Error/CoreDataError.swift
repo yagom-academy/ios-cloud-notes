7699d71c1a187e4f83447514bfb0dde2a9727c95
fezravien <yjaewoong@naver.com>
2021-06-14 21:01:20 +0900

    ✨ [feat] : 코어 데이터 Error 타입 구현

---
+++ CloudNotes/CloudNotes/Error/CoreDataError.swift
@@ -0,0 +1,21 @@
+//
+//  CoreDataError.swift
+//  CloudNotes
+//
+//  Created by 윤재웅 on 2021/06/13.
+//
+
+import Foundation
+
+enum CoreDataError {
+    case fetch(Error?)
+}
+
+extension CoreDataError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .fetch(let error):
+            return "⛔️ CoreData fetch Error: \(String(describing: error))"
+        }
+    }
+}

6775d6dcb4fc3ba139a6120ae96e6aa5bde1fb5e
fezravien <yjaewoong@naver.com>
2021-06-14 21:00:42 +0900

    🔧 [build] : 코어 데이터 파일 재생성

---
+++ CloudNotes/CloudNotes/Model/Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  CloudNotes
+//
+//  Created by 윤재웅 on 2021/06/13.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}

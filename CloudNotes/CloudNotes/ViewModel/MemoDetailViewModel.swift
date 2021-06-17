88aa584bb3064808fa174420edbf11f36755e9a1
kane-young <lyw2100@naver.com>
2021-06-17 21:22:49 +0900

    Refactor : title, body의 유무에 따라서 초기 Detail TextView의 Text 분포를 조정

--- CloudNotes/CloudNotes/ViewModel/MemoDetailViewModel.swift
+++ CloudNotes/CloudNotes/ViewModel/MemoDetailViewModel.swift
@@ -15,10 +15,21 @@ final class MemoDetailViewModel {
   }
   
   var delegate: MemoDetailViewModelDelegate?
-  
   private var memo: Memo?
-  var date: Date { return (memo!.lastModified)! }
-  var content: String { return memo!.title! + ContentConstant.doubleNewLine + memo!.body! }
+  
+  var date: Date {
+    guard let memo = memo else { return Date() }
+    guard let date = memo.lastModified else { return Date() }
+    return date
+  }
+  
+  var content: String {
+    guard let memo = memo else { return ContentConstant.emptyString }
+    let title = memo.title ?? ContentConstant.emptyString
+    let body = memo.body ?? ContentConstant.emptyString
+    if title.isEmpty == true && body.isEmpty == true { return ContentConstant.emptyString }
+    return title + ContentConstant.doubleNewLine + body
+  }
   
   func configure(with memo: Memo) {
     self.memo = memo

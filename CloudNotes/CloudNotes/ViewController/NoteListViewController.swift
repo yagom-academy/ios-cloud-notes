454bd8679f8a625de6a8459a335ecc5f8abcff8c
eunseo5355 <eunseo5355@naver.com>
2021-06-18 21:48:04 +0900

    ✨ [Feat] 메모를 추가, 수정, 삭제, 공유했을때의 테이블뷰 기능 구현
    - 노트 데이터가 추가되거나 삭제될때 테이블뷰의 섹션이 리로드되고 첫번째 메모가 보이도록 구현
    - 메모가 수정되었다는 알림을 받으면 셀과 데이터가 업데이트되도록 구현
    - 메모가 삭제되었다는 알림을 받으면 데이터가 삭제되도록 구현
    - 테이블뷰 스와이프 기능 구현(share, delete)
    - 스와이프해서 delete누르면 삭제할건지 되묻는 alert가 뜨도록 구현
    - 스와이프해서 share누르면 activityVC가 뜨도록 구현
    - 가로가 compact일때만 deselect 되도록 구현

--- CloudNotes/CloudNotes/ViewController/NoteListViewController.swift
+++ CloudNotes/CloudNotes/ViewController/NoteListViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 final class NoteListViewController: UIViewController {
-    private let noteTableView: UITableView = UITableView()
-    private var noteListViewModel: NoteListViewModel = NoteListViewModel()
+    private let noteTableView: UITableView = UITableView(frame: .zero, style: .insetGrouped)
+    let noteListViewModel: NoteListViewModel = NoteListViewModel()
     weak var noteDelegate: NoteDelegate?
     
     override func viewDidLoad() {
@@ -17,10 +17,12 @@ final class NoteListViewController: UIViewController {
         configureTableView()
         configureNavigationItem()
         configureConstraint()
+        addNotification()
         
         noteListViewModel.notes.bind { [weak self] _ in
             DispatchQueue.main.async {
-                self?.noteTableView.reloadData()
+                self?.noteTableView.reloadSections(IndexSet(0...0), with: .automatic)
+                self?.showFirstNote()
             }
         }
     }
@@ -34,13 +36,6 @@ final class NoteListViewController: UIViewController {
         self.noteTableView.showsVerticalScrollIndicator = false
     }
     
-    private func configureNavigationItem() {
-        self.navigationItem.title = "메모"
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
-                                                                 target: self,
-                                                                 action: nil)
-    }
-    
     private func configureConstraint() {
         noteTableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(noteTableView)
@@ -51,6 +46,62 @@ final class NoteListViewController: UIViewController {
             noteTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             noteTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
         ])
+    }
+    
+    private func configureNavigationItem() {
+        self.navigationItem.title = NoteLiteral.navigationItemTitle
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                                 target: self,
+                                                                 action: #selector(didTapAddButton))
+    }
+    
+    private func showFirstNote() {
+        if noteListViewModel.isDataExist(), UITraitCollection.current.horizontalSizeClass == .regular {
+            noteTableView.selectRow(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .none)
+            noteDelegate?.showDetailNote(self, indexPath: [0, 0])
+        }
+    }
+    
+    private func addNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(didChangeNote), name: UITextView.textDidChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(didDeleteNote), name: UITableView.selectionDidChangeNotification, object: nil)
+    }
+    
+    @objc func didChangeNote(_ notification: Notification) {
+        guard let object = notification.object as? [String: Any?],
+              let note = object["note"] as? String,
+              let lastModifiedDate = object["lastModifiedDate"] as? Date,
+              let indexPath = object["indexPath"] as? IndexPath
+        else { return }
+        
+        noteListViewModel.updateNote(note, lastModifiedDate, indexPath)
+        updateCell(note, lastModifiedDate, at: indexPath)
+    }
+    
+    @objc func didDeleteNote(_ notification: Notification) {
+        guard let indexPath = notification.object as? IndexPath else { return }
+        
+        noteListViewModel.deleteNote(indexPath: indexPath)
+    }
+    
+    @objc func didTapAddButton() {
+        noteListViewModel.createNote()
+        noteDelegate?.showDetailNote(self, indexPath: [0, 0])
+    }
+    
+    func updateCell(_ note: String, _ lastModifiedDate: Date, at indexPath: IndexPath) {
+        guard let cell = noteTableView.cellForRow(at: indexPath) as? NoteListTableViewCell else { return }
+        
+        let separatedNotes = note.split(separator: NoteLiteral.LineBreak.Character, maxSplits: 1).map { (value) -> String in
+            return String(value)
+        }
+        if separatedNotes.count == 0 {
+            cell.update(title: NoteLiteral.defaultTitle, date: lastModifiedDate, body: NoteLiteral.defaultBody)
+        } else if separatedNotes.count == 1 {
+            cell.update(title: separatedNotes[NoteLiteral.titleIndex], date: lastModifiedDate, body: NoteLiteral.empty)
+        } else {
+            cell.update(title: separatedNotes[NoteLiteral.titleIndex], date: lastModifiedDate, body: separatedNotes[NoteLiteral.bodyIndex])
+        }
     }
 }
 
@@ -63,7 +114,7 @@ extension NoteListViewController: UITableViewDataSource {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: NoteListTableViewCell.identifier, for: indexPath) as? NoteListTableViewCell
         else { return UITableViewCell() }
         
-        cell.configure(noteListViewModel.getNoteViewModel(for: indexPath))
+        cell.configure(noteListViewModel.getNoteData(for: indexPath))
         
         return cell
     }
@@ -71,7 +122,40 @@ extension NoteListViewController: UITableViewDataSource {
 
 extension NoteListViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        noteDelegate?.showDetailNote(self, data: noteListViewModel.getNoteViewModel(for: indexPath))
+        if UITraitCollection.current.horizontalSizeClass == .compact {
+            tableView.deselectRow(at: indexPath, animated: true)
+        }
+        noteDelegate?.showDetailNote(self, indexPath: indexPath)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let deleteAction = UIContextualAction(style: .destructive, title: NoteLiteral.deleteTitle) { action, view, completionHandler in
+            let alert = UIAlertController(title: nil, message: NoteLiteral.deleteMessage, preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: NoteLiteral.cancelTitle, style: .cancel, handler: nil)
+            let deleteAction = UIAlertAction(title: NoteLiteral.deleteTitle, style: .destructive) { _ in
+                self.noteListViewModel.deleteNote(indexPath: indexPath)
+            }
+            
+            alert.addAction(cancelAction)
+            alert.addAction(deleteAction)
+            
+            self.present(alert, animated: true, completion: nil)
+            
+            completionHandler(true)
+        }
+        
+        let shareAction = UIContextualAction(style: .normal, title: NoteLiteral.shareTitle) { action, view, completionHandler in
+            let shareNote = [self.noteListViewModel.getNote(for: indexPath)]
+            let activityViewController = UIActivityViewController(activityItems: shareNote, applicationActivities: nil)
+            activityViewController.popoverPresentationController?.sourceView = self.view
+            self.present(activityViewController, animated: true, completion: nil)
+            completionHandler(true)
+        }
+        
+        return UISwipeActionsConfiguration(actions: [deleteAction, shareAction])
+    }
+    
+    func tableView(_ tableView: UITableView, shouldSpringLoadRowAt indexPath: IndexPath, with context: UISpringLoadedInteractionContext) -> Bool {
+        return false
     }
 }

fd902cf3f6cd61594c8a590e3ec2df63bf0edd84
KimWanki <seoulkwk95@gmail.com>
2021-08-31 18:07:16 +0900

    feat: MemoPresentModel 타입 구현

---
+++ CloudNotes/CloudNotes/Model/MemoPresentModel.swift
@@ -0,0 +1,14 @@
+//
+//  MemoListPresentModel.swift
+//  CloudNotes
+//
+//  Created by WANKI KIM on 2021/08/31.
+//
+
+import Foundation
+
+struct MemoPresentModel {
+    let title: String
+    let body: String
+    let date: String
+}

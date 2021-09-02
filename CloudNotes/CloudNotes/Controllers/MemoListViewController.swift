3db4e069db3ab27bb6a96b5dd634886ce16125b4
YongHoonJJo <starctak@gmail.com>
2021-09-02 23:51:59 +0900

    feat: MemoList 에 대한 tableView 구현 (programmatically)
    - DataSource 프로토콜 채택 및 준수.
    - Layout Anchors 방식으로 테이블뷰 오토레이아웃 적용.

--- CloudNotes/CloudNotes/Controllers/MemoListViewController.swift
+++ CloudNotes/CloudNotes/Controllers/MemoListViewController.swift
@@ -7,12 +7,43 @@
 import UIKit
 
 class MemoListViewController: UIViewController {
-
+    private let tableView = UITableView()
+    private let memoList = Memo.generateMemoList()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
-
-
+    
+    func setupTableView() {
+        view.addSubview(tableView)
+        
+        let safeArea = view.safeAreaLayoutGuide
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: safeArea.leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: safeArea.rightAnchor).isActive = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        tableView.dataSource = self
+    }
 }
 
+extension MemoListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memoList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let memo = memoList[indexPath.row]
+        cell.textLabel?.text = memo.title
+        cell.accessoryType = .disclosureIndicator
+        
+        return cell
+    }
+}

2e2a128dda4dca188528ea824ea76c01bf6e90b4
Ldoy <leedoy7989@gmail.com>
2021-09-03 00:13:46 +0900

    feat: MainViewController 에 테이블뷰 추가

--- CloudNotes/CloudNotes/Controller/MainViewController.swift
+++ CloudNotes/CloudNotes/Controller/MainViewController.swift
@@ -6,34 +6,16 @@
 
 import UIKit
 
-@IBDesignable
 class MainViewController: UIViewController {
+   let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.addSubview(mainTableView)
-        mainTableView.clipsToBounds = true
-        mainTableView.directionalLayoutMargins = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
-        self.mainTableView.register(UITableViewCell.self, forCellReuseIdentifier: "mainCell")
-        mainTableView.preservesSuperviewLayoutMargins = true
-        mainTableView.directionalLayoutMargins = .zero
-        
-        //mainTableView.dataSource = self
+        view.backgroundColor = .white // 배경색
+        tableView.backgroundColor = .systemGray
+        view.addSubview(tableView)
+        SetLayout.setupTableView(tableView, self.view)
+ 
     }
-    
-    let mainTableView = UITableView()
-    //
 
 }
-
-//extension MainViewController: UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        10
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//
-//    }
-//
-//
-//}

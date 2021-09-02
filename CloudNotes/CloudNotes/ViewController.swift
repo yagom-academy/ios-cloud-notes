4d58caab5752dddfb11599c334e40970db2db421
YongwooKim <keepingitflow@gmail.com>
2021-09-02 22:41:30 +0900

    docs: 스토리보드 삭제후 코드로 UI 구현하기 초기 셋팅

--- CloudNotes/CloudNotes/ViewController.swift
+++ CloudNotes/CloudNotes/ViewController.swift
@@ -10,6 +10,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let test = UILabel()
+        view.backgroundColor = .white // 배경색
+        view.addSubview(test)
+        test.text = "text" // test를 위해서 출력할 라벨
+        test.textColor = .black
+        test.translatesAutoresizingMaskIntoConstraints = false
+        test.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        test.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
 }

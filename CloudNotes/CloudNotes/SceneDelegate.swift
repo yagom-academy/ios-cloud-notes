a09e6a48b2af0cafb0a269d9354789c4ad5c4233
ohsg0226 <ohsg0226@naver.com>
2021-09-03 23:30:39 +0900

    fix: scenceDelgate파일 오타 수정

--- CloudNotes/CloudNotes/SceneDelegate.swift
+++ CloudNotes/CloudNotes/SceneDelegate.swift
@@ -16,11 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
-                window = UIWindow(windowScene: windowScene)
-                let mainViewController = ViewController()
-                let navigationController = UINavigationController(rootViewController: mainViewController)
-                window?.rootViewController = navigationController
-                window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        let mainViewController = MainViewController()
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

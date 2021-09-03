652b19ee4b0501cb0c6276421440b8be54d70bab
blanche37 <wkddbsghk16@gmail.com>
2021-09-03 15:06:32 +0900

    feat: initialViewController를 SplitViewController로 설정

--- CloudNotes/CloudNotes/SceneDelegate.swift
+++ CloudNotes/CloudNotes/SceneDelegate.swift
@@ -1,8 +1,8 @@
 //
 //  CloudNotes - SceneDelegate.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
 
@@ -15,7 +15,23 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        
+        let splitView = SplitViewController()
+        let menu = MenuViewController()
+        let detail = DetailViewController()
+        
+        let rootNavi:UINavigationController = UINavigationController.init(rootViewController: menu)
+        let detailNavi:UINavigationController = UINavigationController.init(rootViewController: detail)
+        splitView.viewControllers = [rootNavi, detailNavi]
+        splitView.menu = menu
+        splitView.detail = detail
+        menu.splitView = splitView
+        detail.splitView = splitView
+        
+        window?.rootViewController = splitView
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

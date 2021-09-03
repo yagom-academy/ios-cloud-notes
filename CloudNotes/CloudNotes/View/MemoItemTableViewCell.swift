25918d1bb1ca2118b5f6cc9ea1e402907457728f
ictechgy <ictechgy@gmail.com>
2021-09-03 17:34:42 +0900

    feat: 리스트에서 각각의 메모 아이템을 나타낼 커스텀 셀 구현
    * 명세서 스크린샷에 따라 기본적인 Constraint 설정코드 작성

---
+++ CloudNotes/CloudNotes/View/MemoItemTableViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  MemoItemTableViewCell.swift
+//  CloudNotes
+//
+//  Created by JINHONG AN on 2021/09/03.
+//
+
+import UIKit
+
+class MemoItemTableViewCell: UITableViewCell {
+    private let outerStackView = UIStackView()
+    private let innerStackView = UIStackView()
+    private let titleLabel = UILabel()
+    private let lastModifiedLabel = UILabel()
+    private let summaryLabel = UILabel()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpInnerStackView()
+        setUpOuterStackView()
+        setUpAccessoryView()
+    }
+}
+
+//MARK:- Set View Components
+extension MemoItemTableViewCell {
+    private func setUpInnerStackView() {
+        innerStackView.axis = .horizontal
+        innerStackView.addArrangedSubview(lastModifiedLabel)
+        innerStackView.addArrangedSubview(summaryLabel)
+        innerStackView.distribution = .fill
+        innerStackView.alignment = .fill
+        innerStackView.spacing = 20
+    }
+
+    private func setUpOuterStackView() {
+        let marginGuide = contentView.layoutMarginsGuide
+        outerStackView.topAnchor.constraint(equalTo: marginGuide.topAnchor).isActive = true
+        outerStackView.bottomAnchor.constraint(equalTo: marginGuide.bottomAnchor).isActive = true
+        outerStackView.leadingAnchor.constraint(equalTo: marginGuide.leadingAnchor).isActive = true
+        outerStackView.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor).isActive = true
+        
+        outerStackView.axis = .vertical
+        outerStackView.addArrangedSubview(titleLabel)
+        outerStackView.addArrangedSubview(innerStackView)
+        outerStackView.distribution = .fill
+        outerStackView.alignment = .fill
+        outerStackView.spacing = 4
+    }
+    
+    private func setUpAccessoryView() {
+        accessoryType = AccessoryType.disclosureIndicator
+    }
+}

ba0b2d8123c130a018751b6147807f594758382a
blanche37 <wkddbsghk16@gmail.com>
2021-09-03 16:11:33 +0900

    feat: textView 오토레이아웃 적용

--- CloudNotes/CloudNotes/DetailViewController.swift
+++ CloudNotes/CloudNotes/DetailViewController.swift
@@ -11,4 +11,21 @@ class DetailViewController: UIViewController {
     var splitView: UISplitViewController?
     
     let textView = UITextView()
+    
+    func configureTextView() {
+        view.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        
+        textView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 0).isActive = true
+        textView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: 0).isActive = true
+        textView.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 0).isActive = true
+        textView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: 0).isActive = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTextView()
+    }
 }

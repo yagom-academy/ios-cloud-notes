239c4060423b28ec518136fd31a08314b3afc587
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-03 22:51:09 +0900

    feat: UISplitViewControllerDelegate 채택
    * splitViewController(svc:proposedTopColumn) -> UISplitViewController.Column 메써드를 사용하여 인터페이스가 축소되었을떄 첫화면을 primary Column을 보여주도록 설정
    * splitView의 DisplayMode를 oneBesideSecondary로 설정

--- CloudNotes/CloudNotes/Controller/MemoSplitViewController.swift
+++ CloudNotes/CloudNotes/Controller/MemoSplitViewController.swift
@@ -11,13 +11,21 @@ class MemoSplitViewController: UISplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.preferredDisplayMode = .oneBesideSecondary
+        show(MemoListViewController(), sender: MemoDetailViewController.self)
         assignColumnsToChildVC(primary: MemoListViewController(), secondary: MemoDetailViewController(), supplementary: nil)
+        self.delegate = self
     }
     
     private func assignColumnsToChildVC(primary: UIViewController, secondary: UIViewController, supplementary: UINavigationController?) {
         setViewController(primary, for: .primary)
         setViewController(secondary, for: .secondary)
         setViewController(supplementary, for: .supplementary)
+    }
+}
+
+extension MemoSplitViewController: UISplitViewControllerDelegate {
+    func splitViewController(_ svc: UISplitViewController, topColumnForCollapsingToProposedTopColumn proposedTopColumn: UISplitViewController.Column) -> UISplitViewController.Column {
+        return .primary
     }
 }

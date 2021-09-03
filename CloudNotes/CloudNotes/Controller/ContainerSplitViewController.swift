09fb928997a026b560d74b6d33d456c1f0523a80
ictechgy <ictechgy@gmail.com>
2021-09-03 19:11:07 +0900

    feat: ContainerSplitViewController에 내부 VC 추가하는 코드 작성
    * required init?(coder:)를 override하여 이 entry 뷰 컨트롤러가
      스토리보드를 통해 initialize될 때 스타일을 설정해주는 initalizer를
      호출하도록 만듦. 이를 통해 column이 설정된 SplitViewController가
      만들어지도록 함.
      (UISplitViewController가 스타일을 가지려면 style에 대한 initializer
      로 만들어져야 함. 이렇게 안하니 아래와 같은 오류 발생!)
      This API requires initializing the UISplitViewController with -initWithStyle:

--- CloudNotes/CloudNotes/Controller/ContainerSplitViewController.swift
+++ CloudNotes/CloudNotes/Controller/ContainerSplitViewController.swift
@@ -7,12 +7,26 @@
 import UIKit
 
 class ContainerSplitViewController: UISplitViewController {
-
+    private let columnStyle = UISplitViewController.Style.doubleColumn
+    
+    required init?(coder: NSCoder) {
+        super.init(style: columnStyle)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        embedViewControllers()
     }
-
 
 }
 
+//MARK:- Embed Inner ViewControllers
+extension ContainerSplitViewController {
+    private func embedViewControllers() {
+        let primaryViewController = MemoListViewController()
+        setViewController(primaryViewController, for: .primary)
+        
+        let secondaryViewController = MemoDetailViewController()
+        setViewController(secondaryViewController, for: .secondary)
+    }
+}

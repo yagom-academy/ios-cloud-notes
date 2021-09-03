b2c3ab843f89e411a517ac0cf9ebb3f50f576fd7
blanche37 <wkddbsghk16@gmail.com>
2021-09-03 15:25:55 +0900

    feat: ParsingManager 타입 구현
    import UIKit
    parse(fileName:) : 파일이름을 입력받아 에셋에 있으면 파싱

--- CloudNotes/CloudNotes/ParsingManager.swift
+++ CloudNotes/CloudNotes/ParsingManager.swift
@@ -5,8 +5,20 @@
 //  Created by yun on 2021/09/03.
 //
 
-import Foundation
+import UIKit
 
 struct ParsingManager {
+    private let jsonDecoder = JSONDecoder()
     
+    func parse<T: Decodable>(fileName: String) -> [T] {
+        guard let dataAsset = NSDataAsset(name: fileName) else {
+            fatalError()
+        }
+        do {
+            let result = try jsonDecoder.decode([T].self, from: dataAsset.data)
+            return result
+        } catch {
+            fatalError()
+        }
+    }
 }

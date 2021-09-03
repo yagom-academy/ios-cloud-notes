fe84df9215ae553973e67095aabd21950ebd85d0
ictechgy <ictechgy@gmail.com>
2021-09-03 23:37:32 +0900

    feat: 파싱관련 커스텀 매니저 타입 정의
    * Controller쪽에서 직접적으로 do-catch를 쓰지 않고 Result를 쓰게 하도록
      파싱관련 ParsingManager 타입 구현
    * 참조될 필요가 없다고 생각했기에 해당 타입은 struct로 구현
    * 내부에서 가지고 있어야 할 프로퍼티는 없다고 판단하여 static 메서드로
      기능 구현

---
+++ CloudNotes/CloudNotes/Model/Manager/ParsingManager.swift
@@ -0,0 +1,19 @@
+//
+//  ParsingManager.swift
+//  CloudNotes
+//
+//  Created by JINHONG AN on 2021/09/03.
+//
+
+import Foundation
+
+struct ParsingManager<T: Decodable> {
+    static func decode(from data: Data) -> Result<T, Error> {
+        do {
+            let decodingResult = try JSONDecoder().decode(T.self, from: data)
+            return .success(decodingResult)
+        } catch {
+            return .failure(error)
+        }
+    }
+}

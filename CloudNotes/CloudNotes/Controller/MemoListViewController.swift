9fd029620f5280cdc7df2c8ee9e9214e474ced8b
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-03 23:31:39 +0900

    feat: 네비게이션 타이틀 추가

--- CloudNotes/CloudNotes/Controller/MemoListViewController.swift
+++ CloudNotes/CloudNotes/Controller/MemoListViewController.swift
@@ -8,10 +8,11 @@
 import UIKit
 
 class MemoListViewController: UIViewController {
-
+    private let navigationTitle = "메모"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.navigationItem.title = navigationTitle
         // Do any additional setup after loading the view.
     }
     

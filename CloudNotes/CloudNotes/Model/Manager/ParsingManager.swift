64778c76b22783d4bd7e97cf546c8ff102b4abba
ictechgy <ictechgy@gmail.com>
2021-09-04 02:42:25 +0900

    refactor: ParsingManager 생성자 private화
    * 인스턴스가 생성될 필요가 없으므로 private로 생성자를 막음.
    * 이럴 때는 case-less enum을 쓸 수도 있지만 열거형이라는게 의미가
      맞지는 않는 것 같아서 enum을 쓰지는 않았다.

--- CloudNotes/CloudNotes/Model/Manager/ParsingManager.swift
+++ CloudNotes/CloudNotes/Model/Manager/ParsingManager.swift
@@ -8,6 +8,9 @@
 import Foundation
 
 struct ParsingManager<T: Decodable> {
+    
+    private init() {}
+    
     static func decode(from data: Data) -> Result<T, Error> {
         do {
             let decodingResult = try JSONDecoder().decode(T.self, from: data)

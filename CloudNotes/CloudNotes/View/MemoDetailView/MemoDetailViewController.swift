4681eb56ba3eaaa90c9ac1c3e0f92978929b64d0
KimWanki <seoulkwk95@gmail.com>
2021-09-03 18:05:54 +0900

    feat: 디렉토리 파일 구조 변경 ViewController 내부 메서드 구현
    Memorizable 프로토콜 선언 및 내부 메서드 구현
    View 폴더 내부로  파일 이동

---
+++ CloudNotes/CloudNotes/View/MemoDetailView/MemoDetailViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MemoDetailViewController.swift
+//  CloudNotes
+//
+//  Created by Luyan on 2021/08/31.
+//
+
+import UIKit
+
+protocol Memorizable: NSObject {
+    func saveMemo(with newMemo: Memo, index: Int)
+}
+
+class MemoDetailViewController: RootViewController {
+    weak var delegate: Memorizable?
+    private var memo: Memo?
+    private var index: Int?
+    private let memoView = MemoDetailView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureTextField()
+    }
+
+    override func setup() {
+        self.view = memoView
+        memoView.detailTextView.delegate = self
+    }
+
+    func configure(with memo: Memo, index: Int) {
+        self.memo = memo
+        self.index = index
+    }
+}
+
+private extension MemoDetailViewController {
+    func configureTextField() {
+        memo.flatMap { memoView.detailTextView.text = $0.title + "\n\n" + $0.body }
+        scrollToTop()
+    }
+
+    func scrollToTop() {
+        let contentHeight = memoView.detailTextView.contentSize.height
+        let offSet = memoView.detailTextView.contentOffset.x
+        let contentOffset = contentHeight - offSet
+        memoView.detailTextView.contentOffset = CGPoint(x: 0, y: -contentOffset)
+    }
+}
+
+extension MemoDetailViewController: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        guard let index = index else { return }
+        let minumumLine = 3
+        let title = textView.text.lines[0]
+        var body = ""
+        if minumumLine <= textView.text.lines.count {
+            body = textView.text.lines[(minumumLine - 1)...].joined(separator: "\n")
+        }
+        delegate?.saveMemo(with: Memo(title: title, body: body, lastDate: Date().timeIntervalSince1970), index: index)
+    }
+}
--- CloudNotes/CloudNotes/View/MemoListView/MemoListCell.swift
+++ CloudNotes/CloudNotes/View/MemoListView/MemoListCell.swift
@@ -1,5 +1,5 @@
 //
-//  memoListCell.swift
+//  MemoListCell.swift
 //  CloudNotes
 //
 //  Created by Luyan on 2021/08/31.
@@ -9,7 +9,48 @@ import UIKit
 
 class MemoListCell: UITableViewCell {
     static let reuseIdentifier = "\(MemoListCell.self)"
-    
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+
+    lazy var bodyLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .gray
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+
+    lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.adjustsFontForContentSizeCategory = true
+        return label
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.accessoryType = .disclosureIndicator
+        setupUI()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with memo: MemoPresentModel) {
+        self.titleLabel.text = memo.title
+        self.dateLabel.text = memo.date
+        self.bodyLabel.text = memo.body
+    }
+}
+
+// MARK: - Layout
+extension MemoListCell {
     private enum Constraint {
         enum TitleLabel {
             static let leadingConstant: CGFloat = 5
@@ -26,58 +67,11 @@ class MemoListCell: UITableViewCell {
         }
     }
 
-    var titleLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .title1)
-        label.adjustsFontForContentSizeCategory = true
-        return label
-    }()
-    var bodyLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .gray
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        label.adjustsFontForContentSizeCategory = true
-        return label
-    }()
-    var dateLabel: UILabel = {
-        let label = UILabel()
-        label.font = UIFont.preferredFont(forTextStyle: .body)
-        label.adjustsFontForContentSizeCategory = true
-        return label
-    }()
-
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        self.accessoryType = .disclosureIndicator
-        setConstraint()
-    }
-
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
-    func configure(with memo: MemoPresentModel) {
-        self.titleLabel.text = memo.title
-        self.dateLabel.text = memo.date
-        self.bodyLabel.text = memo.body
-    }
-
-
-}
-
-// MARK: - Layout
-extension MemoListCell {
-    private func setConstraint() {
-        contentView.addSubview(titleLabel)
-        contentView.addSubview(bodyLabel)
-        contentView.addSubview(dateLabel)
+    private func setupUI() {
+        contentView.addSubviews(titleLabel, bodyLabel, dateLabel)
 
         dateLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
         bodyLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        dateLabel.translatesAutoresizingMaskIntoConstraints = false
-        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
 
         var cellConstraints: [NSLayoutConstraint] = []
         let titleLabelConstraints = [titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor,

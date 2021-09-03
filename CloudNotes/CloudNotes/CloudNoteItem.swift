e4b6b17db90ae1a5616603e3449ab31313b72664
blanche37 <wkddbsghk16@gmail.com>
2021-09-03 15:21:38 +0900

    feat: CloudNoteItem 타입 구현
    CodingKeys 추가

--- CloudNotes/CloudNotes/CloudNoteItem.swift
+++ CloudNotes/CloudNotes/CloudNoteItem.swift
@@ -8,5 +8,12 @@
 import Foundation
 
 struct CloudNoteItem: Decodable {
+    let title: String
+    let body: String
+    let lastModified: Int
     
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
 }

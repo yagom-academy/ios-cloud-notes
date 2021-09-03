81a8b5c2b8694f3b35f45a1fb870840be4407e07
ohsg0226 <ohsg0226@naver.com>
2021-09-03 23:06:38 +0900

    feat: MenuTableVC 생성
    - memoList: Memo data를 담을수잇는 배열
    - memoList에 담긴 정보를 각 tableview cell label에 추가

---
+++ CloudNotes/CloudNotes/Controller/MainViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MainViewController.swift
+//  CloudNotes
+//
+//  Created by 오승기 on 2021/09/03.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ CloudNotes/CloudNotes/Controller/MenuTableViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MenuTableViewController.swift
+//  CloudNotes
+//
+//  Created by 오승기 on 2021/09/03.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+    
+    var memoList = [Memo]()
+    
+    override init(style: UITableView.Style) {
+        super.init(style: style)
+        title = "메모"
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(MemoListCell.self,
+                           forCellReuseIdentifier: MemoListCell.identifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return memoList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy.MM.dd"
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MemoListCell.identifier, for: indexPath) as? MemoListCell else {
+            return UITableViewCell()
+        }
+        cell.titleLabel.text = memoList[indexPath.row].title
+        cell.dateLabel.text = dateFormatter.string(from: memoList[indexPath.row].lastModifiedDate)
+        cell.descriptionLabel.text = memoList[indexPath.row].description
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+}

e7e875a27a357bd796ad76138bf9b1cb4a90e73a
ellenJ <jcrescent61@gmail.com>
2021-09-03 17:12:56 +0900

    refactor: CloudNoteSplitViewController로 클래스명 변경 및 스플릿뷰 상속

---
+++ CloudNotes/CloudNotes/CloudNoteSplitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CloudNotes - ViewController.swift
+//  Created by yagom. 
+//  Copyright © yagom. All rights reserved.
+// 
+
+import UIKit
+
+class CloudNoteSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}

e6dba738a43f21542a11c22b5f0becfa70e55ad6
YongwooKim <keepingitflow@gmail.com>
2021-09-03 16:37:55 +0900

    feat: Model 샘플데이터 추가 및 Memo 모델 구현

---
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -0,0 +1,12 @@
+//
+//  Memo.swift
+//  CloudNotes
+//
+//  Created by Yongwoo Marco on 2021/09/03.
+//
+
+struct Memo {
+    var title: String
+    var body: String?
+    var lastModified: Double
+}

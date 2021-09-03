0fc8d647405ec2bf3577cf4a8b51be50c7debeff
ohsg0226 <ohsg0226@naver.com>
2021-09-03 23:26:53 +0900

    feat: cell 클릭시 동작 추가

--- CloudNotes/CloudNotes/Controller/MenuTableViewController.swift
+++ CloudNotes/CloudNotes/Controller/MenuTableViewController.swift
@@ -48,4 +48,11 @@ class MenuTableViewController: UITableViewController {
         cell.accessoryType = .disclosureIndicator
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let detailView = DetailTextViewController()
+        detailView.memoData = memoList[indexPath.row]
+        self.navigationController?.pushViewController(detailView, animated: true)
+    }
 }

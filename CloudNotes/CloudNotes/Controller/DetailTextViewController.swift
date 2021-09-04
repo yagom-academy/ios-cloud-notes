a23191e02b6b8ce06c7cb85e62ff2932c28f991d
soll4u <dasollpark@gmail.com>
2021-09-04 14:46:05 +0900

    feat: UIScrollView를 이용한 DetailTextView 구현
    * title과 body를 분리

--- CloudNotes/CloudNotes/Controller/DetailTextViewController.swift
+++ CloudNotes/CloudNotes/Controller/DetailTextViewController.swift
@@ -10,33 +10,61 @@ import UIKit
 class DetailTextViewController: UIViewController {
     var memo: Savable?
     
-    private lazy var textView: UITextView = {
+    private lazy var titleTextView: UITextView = {
         let textView = UITextView()
         
-        textView.font = UIFont.preferredFont(forTextStyle: .body)
-        textView.text = memo?.body
+        textView.font = UIFont.preferredFont(forTextStyle: .title3)
+        textView.adjustsFontForContentSizeCategory = true
+        textView.text = memo?.title
+        textView.isScrollEnabled = false
         
         return textView
     }()
     
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.adjustsFontForContentSizeCategory = true
+        textView.text = memo?.body
+        textView.isScrollEnabled = false
+        
+        return textView
+    }()
+    
+    private let scrollView = UIScrollView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.view.addSubview(textView)
-        setTextViewAnchor()
-    }
-    
-    private func setTextViewAnchor() {
-        textView.translatesAutoresizingMaskIntoConstraints = false
         
-        textView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0)
-            .isActive = true
-        textView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0)
-            .isActive = true
-        textView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0)
-            .isActive = true
-        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 0)
-            .isActive = true
+        let safeArea = view.safeAreaLayoutGuide
+        
+        view.addSubview(scrollView)
+        
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+        
+        scrollView.addSubview(titleTextView)
+        
+        titleTextView.translatesAutoresizingMaskIntoConstraints = false
+        titleTextView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor).isActive = true
+        titleTextView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor).isActive = true
+        titleTextView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor).isActive = true
+        titleTextView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
+
+        scrollView.addSubview(bodyTextView)
+
+        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        bodyTextView.topAnchor.constraint(equalTo: titleTextView.bottomAnchor).isActive = true
+        bodyTextView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor).isActive = true
+        bodyTextView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor).isActive = true
+        bodyTextView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor).isActive = true
+        bodyTextView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
+        
     }
     
     func configure(with memo: Savable) {

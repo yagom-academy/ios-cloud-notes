e03c58d12220c57f583608824c6e4107f0e7d34a
YongHoonJJo <starctak@gmail.com>
2021-09-04 21:11:00 +0900

    feat: 음수처리의 경우 autoComplete 기능을 활용하기 위한 Int+Extension 적용
    - nagative 라는 연산 프로퍼티 추가.

---
+++ CloudNotes/CloudNotes/Extensions/Int+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  Int+Extension.swift
+//  CloudNotes
+//
+//  Created by YongHoon JJo on 2021/09/04.
+//
+
+import Foundation
+
+extension Int {
+    var nagative: Int {
+        return self * (-1)
+    }
+}

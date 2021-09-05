97b801e01ccb88d30b8f91324ae8b32a3f02d107
KimWanki <seoulkwk95@gmail.com>
2021-09-05 18:47:05 +0900

    refactor: splitview 생성시점 변경

--- CloudNotes/CloudNotes/AppLifeCycle/SceneDelegate.swift
+++ CloudNotes/CloudNotes/AppLifeCycle/SceneDelegate.swift
@@ -10,9 +10,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         let splitView = SplitViewController()
         
-        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
         window?.rootViewController = splitView

766128d5927966155f944c9b318f27d4ecd88d72
ictechgy <ictechgy@gmail.com>
2021-09-07 02:06:24 +0900

    feat: DataManager타입 구현
    * DataImportable을 채택한 데이터 모듈들로 데이터를 가져오는
      DataManager타입 구현
    * obtainData 메서드에서는 dataImportModule로 가져온 데이터를 처리
      하여 Result타입으로 클로저 호출하도록 작

--- CloudNotes/CloudNotes/Model/Manager/DataManager.swift
+++ CloudNotes/CloudNotes/Model/Manager/DataManager.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+enum DataManagingError: Error, LocalizedError {
+    case unknown
+    
+    var errorDescription: String? {
+        switch self {
+        case .unknown:
+            return "알 수 없는 오류가 발생했습니다."
+        }
+    }
+}
+
+struct DataManager {
+    private let dataImportModule: DataImportable
+    
+    init(dataImportModule: DataImportable) {
+        self.dataImportModule = dataImportModule
+    }
+    
+    func obtainData<T: Decodable>(completionHandler: @escaping (Result<T, Error>) -> Void) {
+        dataImportModule.importData { (data: T?, error: Error?) in
+            if let data = data {
+                completionHandler(.success(data))
+            } else if let error = error {
+                completionHandler(.failure(error))
+            } else {
+                completionHandler(.failure(DataManagingError.unknown))
+            }
+        }
+    }
+}

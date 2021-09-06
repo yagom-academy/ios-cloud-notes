734afa2459c80157263a687f4bb9a19346c7ff7a
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-06 17:40:24 +0900

    feat: 메모의 내용을 나타낼  SampleNotes 구현
    * camelCase 적용할수 있게 CodingKey를 채택

---
+++ CloudNotes/CloudNotes/DataStructure/SampleNotes.swift
@@ -0,0 +1,20 @@
+//
+//  MemoSample.swift
+//  CloudNotes
+//
+//  Created by 김준건 on 2021/09/06.
+//
+
+import Foundation
+
+struct SampleNotes: Decodable {
+    let title: String
+    let body: String
+    let lastModified: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case body
+        case lastModified = "last_modified"
+    }
+}

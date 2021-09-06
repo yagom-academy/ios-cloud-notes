7ecd17daedf936d98233f75b0fdc267badeb055c
soll4u <dasollpark@gmail.com>
2021-09-06 22:48:09 +0900

    refactor: DetailTextViewController 내의 메서드 분리
    * Auto Layout과 BackgroundColor 관련된 메서드 분리
    * Mark 주석 추가
    * 접근제어자 설정

--- CloudNotes/CloudNotes/Controller/DetailTextViewController.swift
+++ CloudNotes/CloudNotes/Controller/DetailTextViewController.swift
@@ -9,7 +9,10 @@ import UIKit
 
 class DetailTextViewController: UIViewController {
     
-    var memo: Savable?
+    // MARK: - Properties
+    private var memo: Savable?
+    
+    private let scrollView = UIScrollView()
     
     private lazy var titleTextView: UITextView = {
         let textView = UITextView()
@@ -33,42 +36,60 @@ class DetailTextViewController: UIViewController {
         return textView
     }()
     
-    private let scrollView = UIScrollView()
-    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(scrollView)
+        changeBackgroundColor(of: scrollView, by: .white)
+        configureScrollViewAnchor()
+        
+        
+        scrollView.addSubview(titleTextView)
+        configureTitleViewAnchor()
+        
+
+        scrollView.addSubview(bodyTextView)
+        configureBodyViewAnchor()
+    }
+    
+    // MARK: - Methods
+    func configure(with memo: Savable) {
+        self.memo = memo
+    }
+    
+    private func changeBackgroundColor(of view: UIView, by color: UIColor) {
+        view.backgroundColor = color
+    }
+    
+    // MARK: - Auto Layout
+    private func configureScrollViewAnchor() {
         let safeArea = view.safeAreaLayoutGuide
         
-        view.addSubview(scrollView)
-        
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        scrollView.backgroundColor = .white
+        
         scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
         scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
         scrollView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
         scrollView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
-        
-        scrollView.addSubview(titleTextView)
-        
+    }
+    
+    private func configureTitleViewAnchor() {
         titleTextView.translatesAutoresizingMaskIntoConstraints = false
+        
         titleTextView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor).isActive = true
         titleTextView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor).isActive = true
         titleTextView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor).isActive = true
         titleTextView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
-
-        scrollView.addSubview(bodyTextView)
-
+    }
+    
+    private func configureBodyViewAnchor() {
         bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        
         bodyTextView.topAnchor.constraint(equalTo: titleTextView.bottomAnchor).isActive = true
         bodyTextView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor).isActive = true
         bodyTextView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor).isActive = true
         bodyTextView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor).isActive = true
         bodyTextView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor).isActive = true
-        
-    }
-    
-    func configure(with memo: Savable) {
-        self.memo = memo
     }
 }

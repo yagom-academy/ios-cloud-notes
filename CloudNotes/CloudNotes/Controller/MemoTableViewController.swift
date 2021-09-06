f70c57d6164dbf24481596eaa4920e97331e20f6
soll4u <dasollpark@gmail.com>
2021-09-07 03:19:30 +0900

    feat: sample JSON 파싱하는 기능 추가
    * Savabler과 Decodable을 채택하는 Memo 타입 구현
    * MemoTableViewController의 memos 프로퍼티에 데이터를 파싱하여 저장

--- CloudNotes/CloudNotes/Controller/MemoTableViewController.swift
+++ CloudNotes/CloudNotes/Controller/MemoTableViewController.swift
@@ -11,6 +11,7 @@ class MemoTableViewController: UITableViewController {
     
     // MARK: - Properties
     var mockItems: [Savable] = [MockModel(), MockModel(), MockModel(),]
+    var memos: [Savable]?
     let isCompact: Bool
     
     // MARK: - Initializer
@@ -27,31 +28,53 @@ class MemoTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        memos = decodeData()
+        
         self.navigationItem.title = "메모"
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
-                                                                    target: self,
-                                                                    action: nil)
-
+                                                                 target: self,
+                                                                 action: nil)
+        
         tableView.register(MemoTableViewCell.self, forCellReuseIdentifier: "MemoTableViewCell")
         tableView.translatesAutoresizingMaskIntoConstraints = false
-
+        
         tableView.estimatedRowHeight = 100
         tableView.rowHeight = UITableView.automaticDimension
+    }
+    
+    func decodeData() -> [Savable]? {
+        let decoder = JSONDecoder()
+        let memoDataIdentifier = "sample"
+        
+        guard let dataAsset = NSDataAsset(name: memoDataIdentifier) else {
+            return nil
+        }
+        
+        if let memos = try? decoder.decode([Memo].self, from: dataAsset.data) {
+            return memos
+        }
+        return nil
     }
 }
 
 // MARK: - Table view data source
 extension MemoTableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return mockItems.count
+        guard let memos = memos else {
+            return 1
+        }
+        return memos.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cellID = "MemoTableViewCell"
         if let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
             as? MemoTableViewCell{
             
-            cell.configureLabels(with: mockItems[indexPath.row])
+            guard let memos = memos else {
+                return UITableViewCell()
+            }
+            cell.configureLabels(with: memos[indexPath.row])
             
             return cell
         }
@@ -65,10 +88,16 @@ extension MemoTableViewController {
         let detailViewController = MemoDetailViewController()
         
         if self.isCompact {
-            detailViewController.configure(with: mockItems[indexPath.row])
+            guard let memos = memos else {
+                return
+            }
+            detailViewController.configure(with: memos[indexPath.row])
             self.navigationController?.pushViewController(detailViewController, animated: true)
         } else {
-            detailViewController.configure(with: mockItems[indexPath.row])
+            guard let memos = memos else {
+                return
+            }
+            detailViewController.configure(with: memos[indexPath.row])
             let navigation = UINavigationController(rootViewController: detailViewController)
             self.showDetailViewController(navigation, sender: self)
         }
---
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -0,0 +1,19 @@
+//
+//  Memo.swift
+//  CloudNotes
+//
+//  Created by Dasoll Park on 2021/09/06.
+//
+
+import Foundation
+
+struct Memo: Savable, Decodable {
+    var title: String?
+    var body: String?
+    var lastModified: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body
+        case lastModified = "last_modified"
+    }
+}

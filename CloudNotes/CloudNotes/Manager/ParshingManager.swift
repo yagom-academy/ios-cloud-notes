f6ba45217c7381dba440b2740519435238b89d9e
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-06 17:52:22 +0900

    feat: ParshingManaer 타입 구현
    * 로컬 파일에 있는 데이터를 파싱할 메서드 구현

---
+++ CloudNotes/CloudNotes/Manager/ParshingManager.swift
@@ -0,0 +1,21 @@
+//
+//  ParshingManager.swift
+//  CloudNotes
+//
+//  Created by 김준건 on 2021/09/06.
+//
+
+import UIKit
+
+struct ParshingManager {
+    private let jsonDecoder = JSONDecoder()
+    
+    func decode<T: Decodable>(from fileName: String, to destinationType: T.Type) {
+        guard let asset = NSDataAsset(name: fileName) else {
+            return
+        }
+        guard let decodedData = try? jsonDecoder.decode(destinationType, from: asset.data) else {
+            return
+        }
+    }
+}

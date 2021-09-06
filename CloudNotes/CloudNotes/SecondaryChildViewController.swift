a68f91c5d48d501fcc7c84a0674f4578df4ed73c
Joey Hong <isjoeyful@gmail.com>
2021-09-06 23:11:55 +0900

    feat: SplitView의 primary와 secondary 뷰 구성
    * PrimaryChildViewController와 SecondaryChildViewController 생성
      * PrimaryChildViewController는 UITableViewController를 상속받도록 구현
    * 각 ViewController를 navigationController에 담아 SplitView의 column에 지정

---
+++ CloudNotes/CloudNotes/SecondaryChildViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondaryChildViewController.swift
+//  CloudNotes
+//
+//  Created by 홍정아 on 2021/09/06.
+//
+
+import UIKit
+
+class SecondaryChildViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- CloudNotes/CloudNotes/SplitViewController.swift
+++ CloudNotes/CloudNotes/SplitViewController.swift
@@ -10,6 +10,17 @@ class SplitViewController: UISplitViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        initChildViewControllers()
+    }
+    
+    func initChildViewControllers() {
+        let primaryViewController = PrimaryChildViewController()
+        let secondaryViewController = SecondaryChildViewController()
+        let primaryChild = UINavigationController(rootViewController: primaryViewController)
+        let secondaryChild = UINavigationController(rootViewController: secondaryViewController)
+        
+        setViewController(primaryChild, for: .primary)
+        setViewController(secondaryChild, for: .secondary)
     }
 }

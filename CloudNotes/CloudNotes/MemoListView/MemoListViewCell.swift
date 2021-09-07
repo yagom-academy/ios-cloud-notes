2f652de2fa7816b901459ce1618ed4d8a2d7f525
soo941226 <83933153+soo941226@users.noreply.github.com>
2021-09-07 18:18:14 +0900

    feat: Added stackView to cell (ing...)

--- CloudNotes/CloudNotes/MemoListView/MemoListViewCell.swift
+++ CloudNotes/CloudNotes/MemoListView/MemoListViewCell.swift
@@ -10,9 +10,9 @@ import UIKit
 class MemoListViewCell: UITableViewCell {
     static let identifier = "MemoListViewCell"
 
-    private lazy var titleLabel = createdTitleLabel()
-    private lazy var dateLabel = createdDateLabel()
-    private lazy var descriptionLabel = createdDescriptionLabel()
+    private var titleLabel: UILabel?
+    private var dateLabel: UILabel?
+    private var descriptionLabel: UILabel?
 
     private let dateFormatter = DateFormatter()
     private let half: CGFloat = 0.5
@@ -35,74 +35,83 @@ class MemoListViewCell: UITableViewCell {
         data = memo
         let lastedUpdatedTime = Date(timeIntervalSince1970: memo.lastUpdatedTime)
 
-        titleLabel.text = memo.title
-        descriptionLabel.text = memo.description
-        dateLabel.text = dateFormatter.string(from: lastedUpdatedTime)
+        titleLabel?.text = memo.title
+        descriptionLabel?.text = memo.description
+        dateLabel?.text = dateFormatter.string(from: lastedUpdatedTime)
     }
 
 }
 
 // MARK: - Draw View
 extension MemoListViewCell {
-
     private var baseLabel: UILabel {
         let label = UILabel()
 
         label.font = UIFont.preferredFont(forTextStyle: .body)
         label.textAlignment = .left
 
-        label.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(label)
 
         return label
     }
 
-    private func createdTitleLabel() -> UILabel {
-        let label = baseLabel
-        label.font = UIFont.preferredFont(forTextStyle: .title2)
+    private func createdStackView(
+        insertedAt superView: UIView? = nil,
+        with contents: UIView?...,
+        axis: NSLayoutConstraint.Axis,
+        spacing: CGFloat,
+        distribution: UIStackView.Distribution,
+        alignment: UIStackView.Alignment
+    ) -> UIStackView {
+        let optionalUnwrappedContents = contents.compactMap { $0 }
+        let stackView = UIStackView(arrangedSubviews: optionalUnwrappedContents)
+        stackView.axis = axis
+        stackView.spacing = spacing
+        stackView.distribution = distribution
+        stackView.alignment = alignment
 
-        label.accessibilityLabel = "제목 : "
+        if let superView = superView {
+            superView.addSubview(stackView)
+        }
 
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: contentView.topAnchor),
-            label.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
-            label.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            label.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: half)
-        ])
-
-        return label
+        return stackView
     }
 
-    private func createdDateLabel() -> UILabel {
-        let label = baseLabel
-        let thirtyHalfPercent: CGFloat = 0.35
+    private func configureLayout(){
+        titleLabel = baseLabel
+        dateLabel = baseLabel
+        descriptionLabel = baseLabel
 
-        label.accessibilityLabel = "날짜 : "
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .title3)
+        dateLabel?.setContentCompressionResistancePriority(.required, for: .horizontal)
+        dateLabel?.setContentHuggingPriority(.required, for: .horizontal)
+
+        let hStackView = createdStackView(
+            with: dateLabel, descriptionLabel,
+            axis: .horizontal,
+            spacing: 12,
+            distribution: .fillProportionally,
+            alignment: .fill
+        )
+
+        let vStackView = createdStackView(
+            insertedAt: contentView,
+            with: titleLabel, hStackView,
+            axis: .vertical,
+            spacing: 6,
+            distribution: .fill,
+            alignment: .fill
+        )
+
+        contentView.addSubview(vStackView)
+
+        vStackView.translatesAutoresizingMaskIntoConstraints = false
 
         NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-            label.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
-            label.widthAnchor.constraint(equalTo: titleLabel.widthAnchor, multiplier: thirtyHalfPercent),
-            label.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: half)
+            vStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            vStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            vStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            vStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor)
         ])
-
-        return label
-    }
-
-    private func createdDescriptionLabel() -> UILabel {
-        let label = baseLabel
-        let sixtyHalfPercent: CGFloat = 0.65
-
-        label.accessibilityLabel = "내용 : "
-        label.textColor = .gray
-
-        NSLayoutConstraint.activate([
-            label.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-            label.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
-            label.widthAnchor.constraint(equalTo: titleLabel.widthAnchor, multiplier: sixtyHalfPercent),
-            label.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: half)
-        ])
-
-        return label
     }
 }

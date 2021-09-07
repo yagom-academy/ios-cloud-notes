ffe852e97d16db8ee2b8e62fcfeebce73e8cc554
Theo <pdhou23@kakao.com>
2021-09-08 06:14:22 +0900

    feat: ContentViewController내 텍스트 뷰 추가, 제약설정, 기기에 따른 텍스트뷰 폰트크기 분기 설정하는 코드 추가

--- CloudNotes/CloudNotes/Controllers/ContentViewController.swift
+++ CloudNotes/CloudNotes/Controllers/ContentViewController.swift
@@ -9,15 +9,31 @@ import UIKit
 
 class ContentViewController: UIViewController {
 
+    var body: String?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        configureView()
+        view.backgroundColor = UIColor.lightGray
         configureNavigationBar()
+        applyAdaptiveLayoutByDevice(textView: configureTextView)
+
     }
 
-    func configureView() {
-        view.backgroundColor = .systemGray4
+    @discardableResult
+    func configureTextView() -> UITextView {
+        let textView = UITextView()
+        textView.backgroundColor = UIColor.lightGray
+        view.addSubview(textView)
+        textView.setConstraintEqualToAnchor(view: self.view)
+        return textView
+    }
+
+    func applyAdaptiveLayoutByDevice(textView: () -> UITextView) {
+        if UITraitCollection.current.userInterfaceIdiom == .phone {
+            textView().font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        } else {
+            textView().font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        }
     }
 
     func configureNavigationBar() {
@@ -26,4 +42,19 @@ class ContentViewController: UIViewController {
         navigationItem.setRightBarButton(rightBarButtonItem, animated: true)
     }
 
+}
+
+extension UIView {
+    func setConstraintEqualToAnchor(view: UIView) {
+        self.translatesAutoresizingMaskIntoConstraints = false
+        let margin = view.layoutMarginsGuide
+        let top = self.topAnchor.constraint(equalTo: margin.topAnchor)
+        let leading = self.leadingAnchor.constraint(equalTo: margin.leadingAnchor)
+        let trailng = self.trailingAnchor.constraint(equalTo: margin.trailingAnchor)
+        let bottom = self.bottomAnchor.constraint(equalTo: margin.bottomAnchor)
+
+        NSLayoutConstraint.activate([
+            top, leading, trailng, bottom
+        ])
+    }
 }

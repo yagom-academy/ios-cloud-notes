02716cad1eac5d7ff0288f74c9cc2ff0b176f48f
ellenJ <jcrescent61@gmail.com>
2021-09-07 10:50:50 +0900

    feat: memoViewController UITextView 오토레이아웃 적용

--- CloudNotes/CloudNotes/MemoViewController.swift
+++ CloudNotes/CloudNotes/MemoViewController.swift
@@ -9,4 +9,17 @@ import UIKit
 
 class MemoViewController: UIViewController {
     
+    var textView: UITextView!
+    
+    override func viewDidLoad() {
+        textView = UITextView()
+        view.addSubview(textView)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        textView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        textView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        textView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        textView.font = UIFont.preferredFont(forTextStyle: .body)
+        textView.text = "wowwowowowowow"
+    }
 }

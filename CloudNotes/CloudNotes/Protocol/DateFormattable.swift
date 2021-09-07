69e06a61d7a40072fab13c5b232403e18d9df36b
Joey Hong <isjoeyful@gmail.com>
2021-09-07 17:55:17 +0900

    feat: DateFormattable 프로토콜 구현
    * TableViewCell이 직접 format할 수 있도록 프로토콜로 구현
    * 사용자의 지역 포맷에 맞게 표현하도록 Locale.current 사용
    * yyyy. MM. dd 날짜 형식 지정

---
+++ CloudNotes/CloudNotes/Protocol/DateFormattable.swift
@@ -0,0 +1,26 @@
+//
+//  DateFormattable.swift
+//  CloudNotes
+//
+//  Created by 홍정아 on 2021/09/07.
+//
+
+import Foundation
+
+protocol DateFormattable {
+    func format(lastModified date: Double) -> String
+}
+
+extension DateFormattable {
+    
+    func format(lastModified date: Double) -> String {
+        let date = Date(timeIntervalSince1970: date)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale.current
+        dateFormatter.dateFormat = "yyyy. MM. dd"
+        
+        return dateFormatter.string(from: date)
+    }
+    
+}

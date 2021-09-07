36d89cc4f97d78f318884dd89f1ee835f225eb60
Joey Hong <isjoeyful@gmail.com>
2021-09-07 17:43:04 +0900

    refactor: Note 모델의 lastModified 타입을 Double로 변경

--- CloudNotes/CloudNotes/Model/Note.swift
+++ CloudNotes/CloudNotes/Model/Note.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Note: Decodable {
     let title: String
     let body: String
-    let lastModified: Int
+    let lastModified: Double
     
     enum CodingKeys: String, CodingKey {
         case title, body

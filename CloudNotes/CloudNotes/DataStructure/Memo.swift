277056fa49b82a300a873f4f6778b23c274e2a95
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-07 15:42:23 +0900

    refactor: Memo타입의 lastModified 데이터 타입 변경
    * TimeInterval은 Double 타입이기에 lastModified 데이터 타입을 Double로 변경

--- CloudNotes/CloudNotes/DataStructure/Memo.swift
+++ CloudNotes/CloudNotes/DataStructure/Memo.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Memo: Decodable {
     let title: String
     let body: String
-    let lastModified: Date
+    let lastModified: Double
     
     enum CodingKeys: String, CodingKey {
         case title

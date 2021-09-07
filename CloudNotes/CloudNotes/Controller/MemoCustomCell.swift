f55d4a2a72c01010482c57279f35f4f9e0b3da28
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-07 19:07:07 +0900

    feat: Label 생성
    * Cell에 필요한 UILabel 생성

--- CloudNotes/CloudNotes/Controller/MemoCustomCell.swift
+++ CloudNotes/CloudNotes/Controller/MemoCustomCell.swift
@@ -8,14 +8,44 @@
 import UIKit
 
 class MemoCustomCell: UITableViewCell {
+    var titleLabel: UILabel!
+    var bodyLabel: UILabel!
+    var lastModifiedLabel: UILabel!
     
     private let cellIdentifier = "CustomCell"
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: cellIdentifier)
+        makeTitleLabel()
+        makeBodyLabel()
+        makeLastModifiedLabel()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+    }
+    
+    private func makeTitleLabel() {
+        titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        titleLabel.textColor = .black
+        titleLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func makeBodyLabel() {
+        bodyLabel = UILabel()
+        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
+        bodyLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        bodyLabel.textColor = .systemGray
+        bodyLabel.adjustsFontForContentSizeCategory = true
+    }
+    
+    private func makeLastModifiedLabel() {
+        lastModifiedLabel = UILabel()
+        lastModifiedLabel.translatesAutoresizingMaskIntoConstraints = false
+        lastModifiedLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        lastModifiedLabel.textColor = .black
+        lastModifiedLabel.adjustsFontForContentSizeCategory = true
     }
 }

f45648289c9a91eeb84e458dfe753c11dc055b4a
Joey Hong <isjoeyful@gmail.com>
2021-09-08 17:07:54 +0900

    fix: UITextView의 title과 body 구하는 로직 수정
    * Note의 내용을 다 지웠을 때 body가 없는데 접근하는 버그 수정
    * body의 index를 알기 위해 title의 index 프로퍼티 추가

--- CloudNotes/CloudNotes/Extension/UITextView+Extension.swift
+++ CloudNotes/CloudNotes/Extension/UITextView+Extension.swift
@@ -12,15 +12,34 @@ extension UITextView {
         return self.text.components(separatedBy: String.lineBreak)
     }
     
-    private var bodyStartIndex: Int {
-        return texts?[1].isEmpty == false ? 1 : 2
+    private var titleStartIndex: Int {
+        if let texts = texts,
+           let firstNonEmptyIndex = texts.firstIndex(where: { $0.isEmpty == false })
+        {
+            let firstNonEmptyIndexToInt = texts.distance(from: texts.startIndex,
+                                                         to: firstNonEmptyIndex)
+            return firstNonEmptyIndexToInt
+        }
+        return .zero
+    }
+    
+    private var bodyStartIndex: Int? {
+        if let textLength = texts?.count,
+           textLength > titleStartIndex + 1
+        {
+            let bodyStartIndex = titleStartIndex + 1
+            return texts?[bodyStartIndex].isEmpty == false ? bodyStartIndex : bodyStartIndex + 1
+        }
+        return nil
     }
     
     var title: String? {
-        return texts?.first
+        return texts?[titleStartIndex]
     }
     
     var body: String? {
+        guard let bodyStartIndex = bodyStartIndex else { return nil }
+        
         return texts?[bodyStartIndex...].joined(separator: String.lineBreak)
     }
 }

65b0e9220c6733aab4e25c1dfb09bbd0a0ed4d40
YongHoonJJo <starctak@gmail.com>
2021-09-09 00:21:51 +0900

    refactor: 첫번째 메모 데이터 바인딩시 예외처리 적용
    - 추가로 코드가 긴 메서드들에 대한 swift style guide 에 의한 인덴트 적용.
    - 뷰컨트롤러, 네비게이션 뷰컨트롤러 관련 네이밍 풀네임으로 변경.

--- CloudNotes/CloudNotes/Controllers/CloudMemoSplitViewController.swift
+++ CloudNotes/CloudNotes/Controllers/CloudMemoSplitViewController.swift
@@ -16,26 +16,41 @@ class CloudMemoSplitViewController: UISplitViewController {
     
     private func initContentHierarchy() {
         let primaryViewController = MemoListViewController()
-        let secondaryViewController = MemoDetailViewController(memo: Memo.generateMemoList()[0])
+        let secondaryViewController = MemoDetailViewController(memo: getFirstMemo())
         
         let primaryNavigationController = UINavigationController(rootViewController: primaryViewController)
         let secondaryNavigationController = UINavigationController(rootViewController: secondaryViewController)
         
         primaryNavigationController.navigationBar.topItem?.title = "메모"
-        primaryNavigationController.navigationBar.topItem?.rightBarButtonItem =  UIBarButtonItem(barButtonSystemItem: .add,
-                                                                       target: self,
-                                                                       action: #selector(addMemoAction))
+        primaryNavigationController.navigationBar.topItem?.rightBarButtonItem =  UIBarButtonItem(
+            barButtonSystemItem: .add,
+            target: self,
+            action: #selector(addMemoAction))
         viewControllers = [primaryNavigationController, secondaryNavigationController]
-        
         self.preferredDisplayMode = .oneBesideSecondary
         self.delegate = self
+    }
+    
+    private func generateNewMemo() -> Memo {
+        let currentTime = Date().timeIntervalSince1970
+        return Memo(title: "새로운 메모", body: "추가 텍스트 없음", lastModified: currentTime)
+    }
+    
+    private func getFirstMemo() -> Memo {
+        guard let memo = Memo.generateMemoList().first else {
+            return generateNewMemo()
+        }
+        
+        return memo
     }
 }
 
 extension CloudMemoSplitViewController: UISplitViewControllerDelegate {
-    func splitViewController(_ splitViewController: UISplitViewController,
-                             collapseSecondary secondaryViewController: UIViewController,
-                             onto primaryViewController: UIViewController) -> Bool {
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,
+        onto primaryViewController: UIViewController
+    ) -> Bool {
         return true
     }
 }
@@ -43,12 +58,12 @@ extension CloudMemoSplitViewController: UISplitViewControllerDelegate {
 extension CloudMemoSplitViewController {
     @objc
     func addMemoAction() {
-        guard let primaryNaviVC = viewControllers.first else {
+        guard let primaryNavigationController = viewControllers.first else {
             return
         }
         
-        let memoDetailVC = MemoDetailViewController(memo: Memo.generateMemoList()[0])
-        let detailNav = UINavigationController(rootViewController: memoDetailVC)
-        primaryNaviVC.showDetailViewController(detailNav, sender: self)
+        let memoDetailViewController = MemoDetailViewController(memo: generateNewMemo())
+        let secondaryNavigationController = UINavigationController(rootViewController: memoDetailViewController)
+        primaryNavigationController.showDetailViewController(secondaryNavigationController, sender: self)
     }
 }

ba3e3c3be767a9e39b973967ccec1696d3aa9707
Ldoy <leedoy7989@gmail.com>
2021-09-09 20:22:42 +0900

    feat: MemoDataHolder 클래스 추가
    - Secondary ViewController로 메모 정보를 전달하는 역할을 하는 MemoDataHolder 구현

--- CloudNotes/CloudNotes/Data/MemoDataHolder.swift
+++ CloudNotes/CloudNotes/Data/MemoDataHolder.swift
@@ -6,3 +6,15 @@
 //
 
 import UIKit
+
+class MemoDataHolder {
+    var testViewText: String?
+    var indexPath: IndexPath?
+    var tableView: UITableView?
+    
+    init(testViewText: String?, indexPath: IndexPath?, tableView: UITableView?) {
+        self.testViewText = testViewText
+        self.indexPath = indexPath
+        self.tableView = tableView
+    }
+}

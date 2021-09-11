f61139a855724b91c21c80d2a2e67c5aac6c1743
Ldoy <leedoy7989@gmail.com>
2021-09-11 21:33:15 +0900

    feat: 코어데이터를 가지고 있는 CoreDataManager 클래스 구현

---
+++ CloudNotes/CloudNotes/Data/CoreDataManager.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  CloudNotes
+//
+//  Created by Do Yi Lee on 2021/09/11.
+//
+
+import UIKit
+import CoreData
+
+class CoreDataManager {
+    static var memos: [Memo]?
+    
+    static let context: NSManagedObjectContext = { () -> NSManagedObjectContext in
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        }
+        let context = appDelegate.persistentContainer.viewContext
+        return context
+    }()
+}

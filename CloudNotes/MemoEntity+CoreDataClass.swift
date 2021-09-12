594639e1131d741ef98faa941c1636a544ee87ab
Hosinging <pdhou23@gmail.com>
2021-09-13 06:19:23 +0900

    feat: 메모Entity 및 attribute생성

---
+++ CloudNotes/MemoEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  MemoEntity+CoreDataClass.swift
+//  CloudNotes
+//
+//  Created by Theo on 2021/09/13.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(MemoEntity)
+public class MemoEntity: NSManagedObject {
+
+}

a8660cdcc5873df2846fda826a61609671ae995f
Hosinging <pdhou23@gmail.com>
2021-09-13 06:19:59 +0900

    feat: DataManager클래스 싱글턴으로 생성

---
+++ CloudNotes/CloudNotes/Shared/DataManager.swift
@@ -0,0 +1,14 @@
+//
+//  DataManager.swift
+//  CloudNotes
+//
+//  Created by Theo on 2021/09/13.
+//
+
+import Foundation
+
+class DataManager {
+    static let shared = DataManager()
+    private init() {}
+
+}

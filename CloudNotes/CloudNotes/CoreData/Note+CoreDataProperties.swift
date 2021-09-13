3c9770d616985cb134d9ac233099f743ef219537
soll4u <dasollpark@gmail.com>
2021-09-14 02:43:42 +0900

    feat: CoreData Note 엔티티 추가

---
+++ CloudNotes/CloudNotes/CoreData/Note+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  Note+CoreDataProperties.swift
+//  
+//
+//  Created by Dasoll Park on 2021/09/14.
+//
+//
+
+import Foundation
+import CoreData
+
+extension Note {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
+        return NSFetchRequest<Note>(entityName: "Note")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var body: String?
+    @NSManaged public var lastModified: Int64
+
+}

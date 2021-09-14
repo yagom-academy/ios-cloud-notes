2e3d2f8b28353565735dd22d27951f31ed74ddf0
ictechgy <ictechgy@gmail.com>
2021-09-14 21:42:41 +0900

    feat: CoreData를 담당할 CoreDataModule타입 기본사항 작성

--- CloudNotes/CloudNotes/Model/Manager/CoreDataModule.swift
+++ CloudNotes/CloudNotes/Model/Manager/CoreDataModule.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import CoreData
+
+struct CoreDataModule {
+    static let basicContainerName = "CloudNotes"
+    static let basicEntityName = "Note"
+    private var persistentContainer: NSPersistentContainer
+    private var context: NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    init(containerName: String = basicContainerName, completionHandler: @escaping (Error?) -> Void) {
+        persistentContainer = NSPersistentCloudKitContainer(name: containerName)
+        persistentContainer.loadPersistentStores { _, error in
+            if let error = error {
+                completionHandler(error)
+            } else {
+                completionHandler(nil)
+            }
+        }
+    }
+}

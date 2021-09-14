776e4a252202593dc0147328cb9ec0b68101826d
ictechgy <ictechgy@gmail.com>
2021-09-14 23:29:02 +0900

    feat: CoreDataModule에 데이터 삽입 메서드 작성
    * entity정보를 가져오지 못했을 경우를 처리하기 위해 CoreDataError 열거형 정의

--- CloudNotes/CloudNotes/Model/Manager/CoreDataModule.swift
+++ CloudNotes/CloudNotes/Model/Manager/CoreDataModule.swift
@@ -8,6 +8,16 @@
 import Foundation
 import CoreData
 
+enum CoreDataError: Error, LocalizedError {
+    case failedToGetEntity
+    var errorDescription: String? {
+        switch self {
+        case .failedToGetEntity:
+            return "데이터베이스 entity 정보를 가져오지 못했습니다."
+        }
+    }
+}
+
 struct CoreDataModule {
     static let basicContainerName = "CloudNotes"
     static let basicEntityName = "Note"
@@ -24,6 +34,24 @@ struct CoreDataModule {
             } else {
                 completionHandler(nil)
             }
+        }
+    }
+    
+    func insert(objectInfo: [String : Any], into entityName: String = basicEntityName, completionHandler: (Error?) -> Void) {
+        guard let entity = NSEntityDescription.entity(forEntityName: entityName, in: context) else {
+            completionHandler(CoreDataError.failedToGetEntity)
+            return
+        }
+        let item = NSManagedObject(entity: entity, insertInto: context)
+        
+        for (key, value) in objectInfo {
+            item.setValue(value, forKey: key)
+        }
+        do {
+            try context.save()
+            completionHandler(nil)
+        } catch  {
+            completionHandler(error)
         }
     }
 }

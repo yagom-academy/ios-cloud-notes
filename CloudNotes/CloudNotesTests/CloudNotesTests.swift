412ab8fa8eed05babf896403e055a7de58b31adb
Joey Hong <isjoeyful@gmail.com>
2021-09-14 14:46:31 +0900

    test: Decodable의 extension에 구현했던 parse()의 테스트 함수 제거

--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -18,28 +18,6 @@ class CloudNotesTests: XCTestCase {
         sut_dateFormatter = nil
     }
 
-    func test_이름이sample인파일의데이터를_parse함수로Note배열타입으로파싱하면_인덱스3인항목의title은네번째다() {
-        // Given
-        let sampleFileName = "sample"
-        let sampleData = NSDataAsset(name: sampleFileName)?.data
-        let expectedValue = "네번째"
-        let targetIndex = 3
-        
-        // When
-        let parsedResult = sampleData?.parse(type: [Note].self)
-        var outcome: String?
-        
-        switch parsedResult {
-        case .success(let parsedData):
-            outcome = parsedData[targetIndex].title
-        default:
-            XCTFail("sample 데이터의 파싱에 실패했습니다")
-        }
-            
-        // Then
-        XCTAssertEqual(expectedValue, outcome)
-    }
-
     func test_DateFormatter로Locale은current고dateType은long으로포맷하면_CurrentLocaleDateFormatter로포맷한것과_같다() {
         // Given
         let date = Date()

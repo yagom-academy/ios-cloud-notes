60fb484cb4a7b4cf09cb9c22aac99002c1710ddf
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-15 21:09:47 +0900

    refactor: configureDebounce 메서드 생성
    * debounce를 구현하여 기존방식 보다 부하를 줄일수 있게 변경

--- CloudNotes/CloudNotes/Controller/MemoDetailViewController.swift
+++ CloudNotes/CloudNotes/Controller/MemoDetailViewController.swift
@@ -11,8 +11,11 @@ class MemoDetailViewController: UIViewController {
     private let memoDeatailTextView = UITextView()
     private var currentMemo: Memo?
     private let linebreak = "\n"
+    private let firstIndex = 1
+    private var workItem: DispatchWorkItem?
     var delegate: MemoSendable?
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(memoDeatailTextView)
@@ -38,15 +41,23 @@ class MemoDetailViewController: UIViewController {
                                      memoDeatailTextView.topAnchor.constraint(equalTo: view.topAnchor),
                                      memoDeatailTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
     }
+    
+    private func configureDebounce(action: @escaping () -> Void) {
+        workItem?.cancel()
+        workItem = DispatchWorkItem(block: action)
+        guard let validWorkItem = workItem else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: validWorkItem)
+    }
 }
 
 extension MemoDetailViewController: UITextViewDelegate {
-    func textViewDidChangeSelection(_ textView: UITextView) {
-        let firstIndex = 1
-        let lastIndex = Int(textView.text.components(separatedBy: linebreak).count - firstIndex)
-        let title = textView.text.components(separatedBy: linebreak)[.zero]
-        let body = textView.text.components(separatedBy: linebreak)[firstIndex...lastIndex].joined(separator: linebreak)
-        let now = Date()
-        delegate?.sendToListVC(memo: Memo.init(title: title, body: body, lastModified: now))
+    func textViewDidChange(_ textView: UITextView) {
+        configureDebounce { [self] in
+            let lastIndex = Int(textView.text.components(separatedBy: linebreak).count - firstIndex)
+            let title = textView.text.components(separatedBy: linebreak)[.zero]
+            let body = textView.text.components(separatedBy: linebreak)[firstIndex...lastIndex].joined(separator: linebreak)
+            let now = Date()
+            delegate?.sendToListVC(memo: Memo.init(title: title, body: body, lastModified: now))
+        }
     }
 }

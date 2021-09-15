fef178ef898b9914f077ba46b6f2333d09bafd81
YongHoonJJo <starctak@gmail.com>
2021-09-16 00:37:44 +0900

    feat: CoreData CRUD 기능 구현
    - 그 외 프로퍼티 및 메서드에 접근제어 추가.

--- CloudNotes/CloudNotes/CoreData/PersistenceManager.swift
+++ CloudNotes/CloudNotes/CoreData/PersistenceManager.swift
@@ -13,7 +13,7 @@ class PersistenceManager {
     
     private init() {}
     
-    var persistentContainer: NSPersistentContainer = {
+    private var persistentContainer: NSPersistentContainer = {
         let container = NSPersistentContainer(name: "Memo")
         container.loadPersistentStores(completionHandler: { (description, error) in
             if let error = error as NSError? {
@@ -23,11 +23,11 @@ class PersistenceManager {
         return container
     }()
     
-    var context: NSManagedObjectContext {
+    private var context: NSManagedObjectContext {
         return self.persistentContainer.viewContext
     }
  
-    func saveContext() {
+    private func saveContext() {
         if context.hasChanges {
             do {
                 try context.save()
@@ -35,5 +35,41 @@ class PersistenceManager {
                 print(error.localizedDescription)
             }
         }
+    }
+    
+    func createMemo(title: String, body: String, lastModified: TimeInterval) {
+        let newMemo = MemoEntity(context: context)
+        newMemo.title = title
+        newMemo.body = body
+        newMemo.lastModified = lastModified
+        
+        saveContext()
+    }
+    
+    func fetchMemo() -> [MemoEntity] {
+        let fetchRequest: NSFetchRequest<MemoEntity> = MemoEntity.fetchRequest()
+        var memoEntities: [MemoEntity] = []
+        
+        do {
+            memoEntities = try context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        return memoEntities
+    }
+    
+    func updateMemo(entity: MemoEntity, title: String, body: String, lastModified: TimeInterval) {
+        entity.title = title
+        entity.body = body
+        entity.lastModified = lastModified
+        
+        saveContext()
+    }
+    
+    func deleteMemo(entity: MemoEntity) {
+        context.delete(entity)
+        
+        saveContext()
     }
 }

098bc703546743eb651b73630ccdb16acf3ee311
jgkim1008 <rlawnsrjs100@naver.com>
2021-09-17 15:41:13 +0900

    refactor: UI요소를 클로저로 생성
    * stackView에서 self를 접근할수 없기에, lazy로 선언
    * layout은 init되는 시점에 제약이 생성되도록 설정

--- CloudNotes/CloudNotes/View/MemoCustomCell.swift
+++ CloudNotes/CloudNotes/View/MemoCustomCell.swift
@@ -8,22 +8,61 @@
 import UIKit
 
 class MemoCustomCell: UITableViewCell {
-    var titleLabel: UILabel!
-    var bodyLabel: UILabel!
-    var lastModifiedLabel: UILabel!
-    var horizontalStackView: UIStackView!
-    var veticalStackView: UIStackView!
+    let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.font = UIFont.preferredFont(forTextStyle: .title2)
+        titleLabel.textColor = .black
+        titleLabel.adjustsFontForContentSizeCategory = true
+        return titleLabel
+    }()
+    
+    let bodyLabel: UILabel = {
+        let bodyLabel = UILabel()
+        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
+        bodyLabel.font = UIFont.preferredFont(forTextStyle: .body)
+        bodyLabel.textColor = .systemGray
+        bodyLabel.adjustsFontForContentSizeCategory = true
+        return bodyLabel
+    }()
+    
+    let lastModifiedLabel: UILabel = {
+        let lastModifiedLabel = UILabel()
+        lastModifiedLabel.translatesAutoresizingMaskIntoConstraints = false
+        lastModifiedLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        lastModifiedLabel.textColor = .black
+        lastModifiedLabel.adjustsFontForContentSizeCategory = true
+        return lastModifiedLabel
+    }()
+    
+    lazy var horizontalStackView: UIStackView = {
+        var horizontalStackView = UIStackView(arrangedSubviews: [lastModifiedLabel, bodyLabel])
+        horizontalStackView.alignment = .fill
+        horizontalStackView.distribution = .fill
+        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
+        horizontalStackView.spacing = 50
+        horizontalStackView.axis = .horizontal
+        contentView.addSubview(horizontalStackView)
+        return horizontalStackView
+    }()
+    
+    lazy var veticalStackView: UIStackView = {
+        var veticalStackView = UIStackView(arrangedSubviews: [titleLabel, horizontalStackView])
+        veticalStackView.alignment = .fill
+        veticalStackView.distribution = .fill
+        veticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        veticalStackView.spacing = 1
+        veticalStackView.axis = .vertical
+        contentView.addSubview(veticalStackView)
+        return veticalStackView
+    }()
     
     static let cellIdentifier = "CustomCell"
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: MemoCustomCell.cellIdentifier)
         self.accessoryType = .disclosureIndicator
-        makeTitleLabel()
-        makeBodyLabel()
-        makeLastModifiedLabel()
-        makeHorizontalStackView()
-        makeVerticalStackView()
+        setHorizontalCompressionResistance()
         setLayoutForStackView()
     }
     
@@ -31,66 +70,20 @@ class MemoCustomCell: UITableViewCell {
         super.init(coder: coder)
     }
     
-    private func makeTitleLabel() {
-        titleLabel = UILabel()
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.font = UIFont.preferredFont(forTextStyle: .title2)
-        titleLabel.textColor = .black
-        titleLabel.adjustsFontForContentSizeCategory = true
-    }
-    
-    private func makeBodyLabel() {
-        bodyLabel = UILabel()
-        bodyLabel.translatesAutoresizingMaskIntoConstraints = false
-        bodyLabel.font = UIFont.preferredFont(forTextStyle: .body)
-        bodyLabel.textColor = .systemGray
-        bodyLabel.adjustsFontForContentSizeCategory = true
-    }
-    
-    private func makeLastModifiedLabel() {
-        lastModifiedLabel = UILabel()
-        lastModifiedLabel.translatesAutoresizingMaskIntoConstraints = false
-        lastModifiedLabel.font = UIFont.preferredFont(forTextStyle: .title3)
-        lastModifiedLabel.textColor = .black
-        lastModifiedLabel.adjustsFontForContentSizeCategory = true
-    }
-    
     private func setHorizontalCompressionResistance() {
         lastModifiedLabel.setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
         bodyLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
     }
     
-    private func makeHorizontalStackView() {
-        setHorizontalCompressionResistance()
-        
-        horizontalStackView = UIStackView(arrangedSubviews: [lastModifiedLabel, bodyLabel])
-        horizontalStackView.alignment = .fill
-        horizontalStackView.distribution = .fill
-        horizontalStackView.translatesAutoresizingMaskIntoConstraints = false
-        horizontalStackView.spacing = 50
-        horizontalStackView.axis = .horizontal
-        contentView.addSubview(horizontalStackView)
-    }
-    
-    private func makeVerticalStackView() {
-        veticalStackView = UIStackView(arrangedSubviews: [titleLabel, horizontalStackView])
-        veticalStackView.alignment = .fill
-        veticalStackView.distribution = .fill
-        veticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        veticalStackView.spacing = 1
-        veticalStackView.axis = .vertical
-        contentView.addSubview(veticalStackView)
-    }
-    
     private func setLayoutForStackView() {
         NSLayoutConstraint.activate([
             veticalStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 10),
-                                     veticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-                                     veticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
-                                     veticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor)
+            veticalStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            veticalStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            veticalStackView.topAnchor.constraint(equalTo: contentView.topAnchor)
         ])
     }
-
+    
     func configureContent(from memo: MemoEntity, with date: String?) {
         titleLabel.text = memo.title
         bodyLabel.text = memo.body

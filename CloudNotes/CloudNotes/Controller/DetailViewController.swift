754371e874896f9918e2652c29ddf1d45be775f2
zoe <kny0651@naver.com>
2022-02-08 14:56:37 +0900

    feat: Detail View UI 구현

--- CloudNotes/CloudNotes/Controller/DetailViewController.swift
+++ CloudNotes/CloudNotes/Controller/DetailViewController.swift
@@ -2,9 +2,40 @@ import UIKit
 
 class DetailViewController: UIViewController {
   
+  private let textView: UITextView = {
+    let textView = UITextView()
+    textView.font = UIFont.preferredFont(forTextStyle: .body)
+    return textView
+  }()
+  
+  var memo: Memo? {
+    didSet {
+      refreshUI()
+    }
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    view.addSubview(textView)
+    textView.translatesAutoresizingMaskIntoConstraints = false
+    textView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+    textView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+    textView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+    textView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    
+    setNavigationBar()
+  }
+  
+  private func refreshUI() {
+    let title = memo?.title ?? ""
+    let body = memo?.body ?? ""
+    textView.text = title + "\n\n" + body
+  }
+  
+  private func setNavigationBar() {
+    let buttonImage = UIImage(systemName: "ellipsis.circle")
+    let ellipsisCircleButton = UIBarButtonItem(image: buttonImage, style: .plain, target: self, action: nil)
+    navigationItem.rightBarButtonItem = ellipsisCircleButton
   }
 }
---
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+struct Memo {
+  let title: String
+  let body: String
+  let lastModified: Date
+}

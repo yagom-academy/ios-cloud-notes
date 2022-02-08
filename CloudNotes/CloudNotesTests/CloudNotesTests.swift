907b41457fd7c83790b8bdbe7c9ebd20f98a15de
yeha <dev.yeha@gmail.com>
2022-02-08 15:07:07 +0900

    style: 테스트 파일 내 주석 제거 #2

--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -1,21 +1,11 @@
-//
-//  CloudNotesTests - CloudNotesTests.swift
-//  Created by yagom. 
-//  Copyright © yagom. All rights reserved.
-// 
-
 import XCTest
 @testable import CloudNotes
 
 class CloudNotesTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
     func test_JSON데이터를_Note타입으로_Deconding하여_인스턴스를_반환해야합니다() throws {
         // given

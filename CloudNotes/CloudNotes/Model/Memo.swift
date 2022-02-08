a96800804eabc5bec80793ff158a96e7bfa58608
zoe <kny0651@naver.com>
2022-02-08 17:50:08 +0900

    refactor: RootViewCell의 subTitle의 body 글자수를 100자로 제한

--- CloudNotes/CloudNotes/Model/Memo.swift
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -11,10 +11,24 @@ extension Memo {
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy. MM. dd. "
     let dateString = dateFormatter.string(from: lastModified)
+    let truncatedBody = body.truncated(limit: 100)
     
     let attributedString = NSMutableAttributedString()
     attributedString.append(NSAttributedString(string: dateString, attributes: [.font: UIFont.preferredFont(forTextStyle: .footnote)]))
-    attributedString.append(NSAttributedString(string: body, attributes: [.font: UIFont.preferredFont(forTextStyle: .caption1), .foregroundColor : UIColor.secondaryLabel]))
+    attributedString.append(NSAttributedString(string: truncatedBody, attributes: [.font: UIFont.preferredFont(forTextStyle: .caption1), .foregroundColor : UIColor.secondaryLabel]))
     return attributedString
+  }
+}
+
+private extension String {
+  func truncated(limit: Int) -> String {
+    let truncatingBody: String
+    if self.count > limit {
+      let endIndex = self.index(self.startIndex, offsetBy: limit)
+      truncatingBody = String(self[...endIndex])
+    } else {
+      truncatingBody = self
+    }
+    return truncatingBody
   }
 }

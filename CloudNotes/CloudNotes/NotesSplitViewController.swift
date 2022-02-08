b2b8f34c72e44a49e8ffea66cb3627abd1682e95
ChaminLee <trueman9512@naver.com>
2022-02-08 11:44:28 +0900

    feat: 스토리보드 제거 및 UISplitViewController 생성
    - Device를 iPad만 지원하도록 설정
    - SceneDelegate내 window 설정
    - ViewController 제거
    - Deployment target 14.0 -> 14.3

---
+++ CloudNotes/CloudNotes/NotesSplitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  NotesSplitViewController.swift
+//  CloudNotes
+//
+//  Created by 이차민 on 2022/02/08.
+//
+
+import UIKit
+
+class NotesSplitViewController: UISplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}

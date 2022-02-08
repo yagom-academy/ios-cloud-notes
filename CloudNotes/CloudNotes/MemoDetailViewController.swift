8fc652805673b3af48285d6a2f8d455e56b3b3ea
Ari <lee_ari95@icloud.com>
2022-02-08 14:26:05 +0900

    feat: MemoDetailViewController에 UITextView를 추가 #1
    - NotificationCenter를 활용하여 TextView에 자동스크롤 기능 구현

--- CloudNotes/CloudNotes/MemoDetailViewController.swift
+++ CloudNotes/CloudNotes/MemoDetailViewController.swift
@@ -1,11 +1,20 @@
 import UIKit
 
 class MemoDetailViewController: UIViewController {
+    private let memoDetailTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = .preferredFont(forTextStyle: .body)
+        textView.adjustsFontForContentSizeCategory = true
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpNavigationItem()
         view.backgroundColor = .systemBackground
+        setUpNavigationItem()
+        setUpTextView()
+        setUpNotification()
     }
     
     private func setUpNavigationItem() {
@@ -15,5 +24,47 @@ class MemoDetailViewController: UIViewController {
             target: self,
             action: nil
         )
+    }
+    
+    private func setUpTextView() {
+        view.addSubview(memoDetailTextView)
+        NSLayoutConstraint.activate([
+            memoDetailTextView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            memoDetailTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            memoDetailTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            memoDetailTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+    
+    private func setUpNotification() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo as NSDictionary?,
+              var keyboardFrame = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        keyboardFrame = view.convert(keyboardFrame, from: nil)
+        var contentInset = memoDetailTextView.contentInset
+        contentInset.bottom = keyboardFrame.size.height
+        memoDetailTextView.contentInset = contentInset
+        memoDetailTextView.scrollIndicatorInsets = memoDetailTextView.contentInset
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        memoDetailTextView.contentInset = UIEdgeInsets.zero
+        memoDetailTextView.scrollIndicatorInsets = memoDetailTextView.contentInset
     }
 }

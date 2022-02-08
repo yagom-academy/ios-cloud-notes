eb4d3d3ab4a7a5b42e3a47595a2534a59977a732
Jager-yoo <y73447jh@gmail.com>
2022-02-08 16:03:26 +0900

    feat: MemoTableViewCell 타입 구현

---
+++ CloudNotes/CloudNotes/View/MemoTableViewCell.swift
@@ -0,0 +1,64 @@
+//
+//  MemoTableViewCell.swift
+//  CloudNotes
+//
+//  Created by 예거 on 2022/02/08.
+//
+
+import UIKit
+
+class MemoTableViewCell: UITableViewCell {
+    static let identifier = "memoTableViewCell"
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .title3)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let dateLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .body)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let previewLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .caption2)
+        label.textColor = .lightGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func configureCell() {
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(previewLabel)
+        self.accessoryType = .disclosureIndicator
+        
+        dateLabel.setContentCompressionResistancePriority(.required, for: .horizontal)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
+            titleLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -5),
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            dateLabel.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            dateLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            dateLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            previewLabel.leadingAnchor.constraint(equalTo: dateLabel.trailingAnchor, constant: 5),
+            previewLabel.trailingAnchor.constraint(equalTo: titleLabel.trailingAnchor),
+            previewLabel.centerYAnchor.constraint(equalTo: dateLabel.centerYAnchor)
+        ])
+    }
+}

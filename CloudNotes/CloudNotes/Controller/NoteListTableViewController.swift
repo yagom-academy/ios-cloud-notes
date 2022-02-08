154351eb3788ece8f77930dcd3d0a11ab4f19e15
Nicholas <83689084+Kim-EunsooSilver@users.noreply.github.com>
2022-02-08 18:19:04 +0900

    feat: 데이터 기반으로 NoteListTableViewController 화면 구현

--- CloudNotes/CloudNotes/Controller/NoteListTableViewController.swift
+++ CloudNotes/CloudNotes/Controller/NoteListTableViewController.swift
@@ -2,21 +2,35 @@ import UIKit
 
 class NoteListTableViewController: UITableViewController {
     
+    let noteModelManager = NoteModelManager()
+    let cellId = "reuseIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
+        noteModelManager.fetchData()
+        self.navigationController?.navigationBar.topItem?.title = "메모"
+        let addBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = addBarButtonItem
     }
     
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 0
+        return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return noteModelManager.noteDataCount
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let noteData = noteModelManager.fetchDetailData(at: indexPath.row)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = noteData.title
+        cell.contentConfiguration = content
         return cell
     }
     

2bafca9a38af3664c749b32bac025729b2fe282d
JeongTaek Han <smart8612@gmail.com>
2022-02-08 15:03:37 +0900

    test: JSONDecoder 테스트 코드 구현 #2

--- CloudNotes/CloudNotesTests/CloudNotesTests.swift
+++ CloudNotes/CloudNotesTests/CloudNotesTests.swift
@@ -17,16 +17,22 @@ class CloudNotesTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func test_JSON데이터를_Note타입으로_Deconding하여_인스턴스를_반환해야합니다() throws {
+        // given
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .secondsSince1970
+        
+        // when
+        let jsonAsset = NSDataAsset(name: "sample")
+        guard let jsonData = jsonAsset?.data else {
+            XCTFail()
+            return
         }
+        
+        // then
+        let result = try? decoder.decode([Note].self, from: jsonData)
+        XCTAssertNotNil(result)
     }
 
 }

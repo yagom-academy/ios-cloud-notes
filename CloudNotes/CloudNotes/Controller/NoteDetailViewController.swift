b68e57372a485a9e369d97fe44287d040864bee1
horeng2 <huski.a.a@gmail.com>
2022-02-08 17:26:44 +0900

    feat: NoteDetailScrollView 추가 #2
    - NoteDetailScrollView 하위에 noteDetailStackView 추가
    - NoteDetailScrollView 하위에 lastModifiedDateLabel,noteDetailTextView 추가
    - view 설정 및 constraint 지정

--- CloudNotes/CloudNotes/Controller/NoteDetailViewController.swift
+++ CloudNotes/CloudNotes/Controller/NoteDetailViewController.swift
@@ -8,21 +8,11 @@
 import UIKit
 
 class NoteDetailViewController: UIViewController {
+    private var noteDetailScrollView = NoteDetailScrollView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.addSubview(noteDetailScrollView)
+        noteDetailScrollView.setupConstraint(view: view)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ CloudNotes/CloudNotes/View/NoteDetailScrollView.swift
@@ -0,0 +1,66 @@
+//
+//  NoteScrollView.swift
+//  CloudNotes
+//
+//  Created by 서녕 on 2022/02/08.
+//
+
+import UIKit
+
+class NoteDetailScrollView: UIScrollView {
+    private var noteDetailStackView = UIStackView()
+    private var lastModifiedDateLabel = UILabel()
+    private var noteDetailTextView = UITextView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupStackView()
+        setupLastModifiedDateLabel()
+        setupNoteDetailTextView()
+        setupStackViewConstraint()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupStackView() {
+        addSubview(noteDetailStackView)
+        noteDetailStackView.axis = .vertical
+        noteDetailStackView.alignment = .fill
+        noteDetailStackView.spacing = 10
+        noteDetailStackView.addArrangedSubview(lastModifiedDateLabel)
+        noteDetailStackView.addArrangedSubview(noteDetailTextView)
+    }
+    
+    func setupLastModifiedDateLabel() {
+        lastModifiedDateLabel.setContentHuggingPriority(.required, for: .vertical)
+        lastModifiedDateLabel.textAlignment = .center
+    }
+    
+    func setupNoteDetailTextView() {
+        noteDetailTextView.isScrollEnabled = false
+    }
+    
+    func setupConstraint(view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: view.topAnchor),
+            bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+    }
+    
+    func setupStackViewConstraint() {
+        noteDetailStackView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            noteDetailStackView.topAnchor.constraint(equalTo: topAnchor),
+            noteDetailStackView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            noteDetailStackView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            noteDetailStackView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            noteDetailStackView.widthAnchor.constraint(equalTo: widthAnchor),
+            noteDetailStackView.heightAnchor.constraint(equalTo: heightAnchor)
+        ])
+    }
+}

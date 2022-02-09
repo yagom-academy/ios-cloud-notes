c2d22c319b5a22fe42cc2e805594f5e095a3baaa
zoe <kny0651@naver.com>
2022-02-09 15:57:36 +0900

    refactor: RootViewController 이니셜라이저 추가

--- CloudNotes/CloudNotes/Controller/RootViewController.swift
+++ CloudNotes/CloudNotes/Controller/RootViewController.swift
@@ -22,6 +22,16 @@ class RootViewController: UICollectionViewController {
     return layout
   }()
   
+  init() {
+    let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
+    let listLayout = UICollectionViewCompositionalLayout.list(using: configuration)
+    super.init(collectionViewLayout: listLayout)
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     collectionView.collectionViewLayout = listLayout
--- CloudNotes/CloudNotes/SceneDelegate.swift
+++ CloudNotes/CloudNotes/SceneDelegate.swift
@@ -13,9 +13,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     
-    let configuration = UICollectionLayoutListConfiguration(appearance: .plain)
-    let listLayout = UICollectionViewCompositionalLayout.list(using: configuration)
-    let rootViewController = RootViewController(collectionViewLayout: listLayout)
+    let rootViewController = RootViewController()
     let detailViewController = DetailViewController()
     rootViewController.delegate = detailViewController
     

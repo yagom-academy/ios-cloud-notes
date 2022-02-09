c2afdc4027ba586c7941cc098fb717ba11263786
aCafela-coffee <acafelacoffee@gmail.com>
2022-02-09 14:15:51 +0900

    style: 줄바꿈 규칙 반영, if문 가독성 개선

--- CloudNotes/CloudNotes/Model/Memo.swift
+++ CloudNotes/CloudNotes/Model/Memo.swift
@@ -14,21 +14,27 @@ extension Memo {
     let truncatedBody = body.truncated(limit: 100)
     
     let attributedString = NSMutableAttributedString()
-    attributedString.append(NSAttributedString(string: dateString, attributes: [.font: UIFont.preferredFont(forTextStyle: .footnote)]))
-    attributedString.append(NSAttributedString(string: truncatedBody, attributes: [.font: UIFont.preferredFont(forTextStyle: .caption1), .foregroundColor : UIColor.secondaryLabel]))
+    attributedString.append(NSAttributedString(
+      string: dateString,
+      attributes: [.font: UIFont.preferredFont(forTextStyle: .footnote)]
+    ))
+    attributedString.append(NSAttributedString(
+      string: truncatedBody,
+      attributes: [
+        .font: UIFont.preferredFont(forTextStyle: .caption1),
+        .foregroundColor : UIColor.secondaryLabel
+      ]
+    ))
     return attributedString
   }
 }
 
 private extension String {
   func truncated(limit: Int) -> String {
-    let truncatingBody: String
     if self.count > limit {
       let endIndex = self.index(self.startIndex, offsetBy: limit)
-      truncatingBody = String(self[...endIndex])
-    } else {
-      truncatingBody = self
+      return String(self[...endIndex])
     }
-    return truncatingBody
+    return self
   }
 }

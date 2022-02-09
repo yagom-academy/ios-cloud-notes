08aad6324ac3ba80ebaf5c81074d16654478858d
wooyani77 <wooyani77@gmail.com>
2022-02-09 16:45:42 +0900

    feat: detailViewController 내부 textView 설정 #2

--- CloudNotes/CloudNotes/Controller/NoteDetailViewController.swift
+++ CloudNotes/CloudNotes/Controller/NoteDetailViewController.swift
@@ -1,10 +1,34 @@
 import UIKit
 
 class NoteDetailViewController: UIViewController {
-
+    private var textView: UITextView = {
+        let textview = UITextView(frame: .zero)
+        textview.font = .preferredFont(forTextStyle: .caption1)
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        return textview
+    }()
+    
+    override func loadView() {
+        view = .init()
+        view.backgroundColor = .white
+        view.addSubview(textView)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        setUpTextViewLayout()
+    }
+    
+    func setUpText(with data: Sample) {
+        textView.text = data.body
+    }
+    
+    private func setUpTextViewLayout() {
+        NSLayoutConstraint.activate([
+            textView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            textView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
 }

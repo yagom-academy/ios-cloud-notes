a6ab0d9cd94fadfb4b781dd9ac182f53d1420d03
yanghojoon <yanghojoon8487@gmail.com>
2022-02-10 15:12:29 +0900

    refactor: SceneDelegate에서 window를 생성하도록 수정 #6

--- CloudNotes/CloudNotes/SceneDelegate.swift
+++ CloudNotes/CloudNotes/SceneDelegate.swift
@@ -10,9 +10,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
         let mainSplitViewController = MainSplitViewController(style: .doubleColumn)
         mainSplitViewController.preferredDisplayMode = .oneBesideSecondary
+        mainSplitViewController.preferredSplitBehavior = .tile
         window?.rootViewController = mainSplitViewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

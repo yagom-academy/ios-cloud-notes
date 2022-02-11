4a8ede63e27900114af709e50e56ce6c7e876dbb
yanghojoon <yanghojoon8487@gmail.com>
2022-02-11 14:32:11 +0900

    refactor: 사용하지 않는 코드를 사용하지 못하도록 수정

--- CloudNotes/CloudNotes/View/MemoListTableViewCell.swift
+++ CloudNotes/CloudNotes/View/MemoListTableViewCell.swift
@@ -48,6 +48,7 @@ final class MemoListTableViewCell: UITableViewCell {
         setupCell()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }

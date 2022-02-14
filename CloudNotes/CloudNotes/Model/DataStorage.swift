8c4eeb4dc568dda37200961331ef1ddb51efc399
yeahg-dev <yeahg.dev@gmail.com>
2022-02-14 23:03:38 +0900

    feat: Memo Entity 생성 및 saveMemoInContext 구현 #5

--- CloudNotes/CloudNotes/Model/DataStorage.swift
+++ CloudNotes/CloudNotes/Model/DataStorage.swift
@@ -1,7 +1,32 @@
 import UIKit
+import CoreData
 
 class DataStorage {
     var assetData: [Sample] {
         return JSONParser.decodeData(of: "sample", how: [Sample].self) ?? [Sample(title: "", body: "", lastModified: .zero)]
+    }
+    
+    private let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext
+    
+    func saveMemoInContext(title: String, body: String, lastModified: Int64) {
+        guard let context = context else { return }
+        
+        guard let entity = NSEntityDescription.entity(
+            forEntityName: "Memo",
+            in: context
+        ) else {
+            return
+        }
+
+        let memo = NSManagedObject(entity: entity, insertInto: context)
+        memo.setValue(title, forKey: "title")
+        memo.setValue(body, forKey: "body")
+        memo.setValue(lastModified, forKey: "lastModified")
+
+        do {
+            try context.save()
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 }
---
+++ CloudNotes/CloudNotes/Model/Memo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Memo+CoreDataClass.swift
+//  
+//
+//  Created by 1 on 2022/02/14.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Memo)
+public class Memo: NSManagedObject {
+
+}

8c9bccd313f7c0e3c2fedf02e829b95f0021930b
July911 <min95911@nate.com>
2022-02-14 15:42:05 +0900

    refactor: JSONParsing 실패 시 nil 을 반환하게 구조 변경 #4

--- CloudNotes/CloudNotes/Model/DataStorage.swift
+++ CloudNotes/CloudNotes/Model/DataStorage.swift
@@ -2,6 +2,6 @@ import UIKit
 
 class DataStorage {
     var assetData: [Sample] {
-        return JSONParser.decodeData(of: "sample", how: [Sample].self)
+        return JSONParser.decodeData(of: "sample", how: [Sample].self) ?? [Sample(title: "", body: "", lastModified: .zero)]
     }
 }
--- CloudNotes/CloudNotes/Utility/JSONParser.swift
+++ CloudNotes/CloudNotes/Utility/JSONParser.swift
@@ -2,14 +2,14 @@ import Foundation
 import UIKit
 
 enum JSONParser {
-    static func decodeData<T: Decodable>(of dataName: String, how: T.Type) -> T {
+    static func decodeData<T: Decodable>(of dataName: String, how: T.Type) -> T? {
         guard let data = NSDataAsset(name: dataName) else {
-            fatalError()
+            return nil
         }
         
         let decoder = JSONDecoder()
         guard let decodedData = try? decoder.decode(how, from: data.data) else {
-            fatalError()
+            return nil
         }
         
         return decodedData

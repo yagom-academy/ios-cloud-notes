3e1abd8f201686545c47ee3b9299085feec4bcdd
horeng2 <huski.a.a@gmail.com>
2022-02-14 18:03:04 +0900

    feat: textView변동 시 title과 body를 조건에 맞게 분할하는 기능 구현

--- CloudNotes/CloudNotes/Controller/NoteDetailViewController.swift
+++ CloudNotes/CloudNotes/Controller/NoteDetailViewController.swift
@@ -21,6 +21,7 @@ final class NoteDetailViewController: UIViewController {
         setupNavigation()
         setupNoteDetailScrollView()
         addObserverKeyboardNotification()
+        noteDetailScrollView.noteDetailTextView.delegate = self
     }
     
     private func setupNavigation() {
@@ -70,6 +71,26 @@ extension NoteDetailViewController: UIScrollViewDelegate {
 
         if scrollView.contentOffset.y < dateLabelHeight {
             targetContentOffset.pointee = CGPoint.zero
+        }
+    }
+}
+
+extension NoteDetailViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        var title = ""
+        var body = ""
+        guard let text = textView.text else {
+            return
+        }
+        if text.contains("\n") {
+            let splitedText = textView.text.split(separator: "\n", maxSplits: 1, omittingEmptySubsequences: true)
+            title = String(splitedText.first ?? "")
+            body = splitedText.last?.trimmingCharacters(in: .newlines) ?? ""
+        } else if text.contains("\n") == false && text.count > 100 {
+            title = text.substring(from: 0, to: 99)
+            body = text.substring(from: 100, to: text.count - 1)
+        } else {
+            title = text
         }
     }
 }
---
+++ CloudNotes/CloudNotes/Extension/String+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  String+Extension.swift
+//  CloudNotes
+//
+//  Created by 서녕 on 2022/02/14.
+//
+
+import UIKit
+
+extension String {
+    func substring(from: Int, to: Int) -> String {
+        guard from < count, to >= 0, to - from >= 0 else {
+            return ""
+        }
+        let startIndex = index(self.startIndex, offsetBy: from)
+        let endIndex = index(self.startIndex, offsetBy: to + 1)
+        
+        return String(self[startIndex ..< endIndex])
+    }
+}

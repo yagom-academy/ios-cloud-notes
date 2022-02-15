f9bfdbe029c81a01dca47701c713d33639369ef1
Yeonhee Ha <dev.yeha@gmail.com>
2022-02-15 10:53:47 +0900

    refactor: SceneDelegate 확장하여 configureSplitView메서드 분리 #4

--- CloudNotes/CloudNotes/Supporting Files/SceneDelegate.swift
+++ CloudNotes/CloudNotes/Supporting Files/SceneDelegate.swift
@@ -3,37 +3,40 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
-    private let noteModelManager: NoteModel = NoteModelManager()
-    private lazy var primaryViewController = NoteListTableViewController(model: noteModelManager)
-    private lazy var secondaryViewController = NoteDetailViewController()
-    private lazy var rootViewController: UISplitViewController = {
-        let splitViewController = UISplitViewController(style: .doubleColumn)
-        splitViewController.view.backgroundColor = .systemBackground
-        return splitViewController
-    }()
-    
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
+        let mainSplitViewController = configureSplitView()
         guard let windowScene = (scene as? UIWindowScene) else {
             return
         }
-        configureRootView()
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = rootViewController
+        window?.rootViewController = mainSplitViewController
         window?.makeKeyAndVisible()
     }
 
     func sceneDidEnterBackground(_ scene: UIScene) {
         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
+
+}
+
+extension SceneDelegate {
     
-    private func configureRootView() {
+    private func configureSplitView() -> UISplitViewController {
+        let noteModelManager: NoteModel = NoteModelManager()
+        let primaryViewController = NoteListTableViewController(model: noteModelManager)
+        let secondaryViewController = NoteDetailViewController()
+        let splitViewController = UISplitViewController(style: .doubleColumn)
+
         primaryViewController.delegate = secondaryViewController
-        rootViewController.setViewController(primaryViewController, for: .primary)
-        rootViewController.setViewController(secondaryViewController, for: .secondary)
+        splitViewController.view.backgroundColor = .systemBackground
+        splitViewController.setViewController(primaryViewController, for: .primary)
+        splitViewController.setViewController(secondaryViewController, for: .secondary)
+
+        return splitViewController
     }
     
 }

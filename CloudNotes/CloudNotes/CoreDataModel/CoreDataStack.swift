9f067e8f617c31302a8dc091edd3bdff79cd9b06
horeng2 <huski.a.a@gmail.com>
2022-02-16 20:40:27 +0900

    feat: CoreDataStack 구현 #3

---
+++ CloudNotes/CloudNotes/CoreDataModel/CoreDataStack.swift
@@ -0,0 +1,39 @@
+//
+//  CoreDataStack.swift
+//  CloudNotes
+//
+//  Created by 서녕 on 2022/02/16.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataStack {
+    static let shared = CoreDataStack()
+    
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "CloudNotes")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    var context: NSManagedObjectContext {
+        return self.persistentContainer.viewContext
+    }
+    
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
--- CloudNotes/CloudNotes/CoreDataModel/Note+CoreDataProperties.swift
+++ CloudNotes/CloudNotes/CoreDataModel/Note+CoreDataProperties.swift
@@ -9,15 +9,12 @@
 import Foundation
 import CoreData
 
-
 extension Note {
     @NSManaged public var title: String?
     @NSManaged public var content: String?
-    @NSManaged public var lastModifiedDate: Double?
+    @NSManaged public var lastModifiedDate: Double
     
     @nonobjc public class func fetchRequest() -> NSFetchRequest<Note> {
         return NSFetchRequest<Note>(entityName: "Note")
     }
-
-
 }

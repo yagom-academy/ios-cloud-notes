f4d9213dd182eeac95856011c240388c9784e53c
wooyani77 <wooyani77@gmail.com>
2022-02-24 22:21:35 +0900

    refactor: 강제언래핑, 코드 위치 수정 및 필요없는 주석 삭제 #8

--- CloudNotes/CloudNotes/Controller/MemoListViewController.swift
+++ CloudNotes/CloudNotes/Controller/MemoListViewController.swift
@@ -13,6 +13,9 @@ final class MemoListViewController: UIViewController {
     weak var delegate: MemoListViewControllerDelegate?
     private let cellIdentifier = "Cell"
     private let tableView = UITableView()
+    var selectedCellIndex: IndexPath? {
+        return tableView.indexPathForSelectedRow
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,6 +55,29 @@ final class MemoListViewController: UIViewController {
     @objc private func addMemo() {
         delegate?.addNewMemo()
     }
+    
+    func setupRowSelection() {
+        tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: false, scrollPosition: .top)
+    }
+    
+    func addNewCell() {
+        tableView.reloadData()
+        tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .top)
+    }
+    
+    func selectNextCell(at indexPath: IndexPath) {
+        tableView.allowsSelectionDuringEditing = true
+        tableView.selectRow(at: indexPath, animated: false, scrollPosition: .top)
+    }
+    
+    func updateCell(at indexPath: IndexPath) {
+        tableView.reloadRows(at: [indexPath], with: .none)
+        tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
+    }
+    
+    func deleteCell(at indexPath: IndexPath) {
+        tableView.deleteRows(at: [indexPath], with: .none)
+    }
 }
 
 // MARK: - UITableViewDataSource
@@ -64,8 +90,10 @@ extension MemoListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
         var configuration = cell.defaultContentConfiguration()
-        let memo = delegate!.getMemo(for: indexPath)
-        configuration.text = memo.title.isEmpty ? "새로운 메모" : memo.title
+        guard let memo = delegate?.getMemo(for: indexPath) else {
+            return UITableViewCell()
+        }
+        configuration.text = memo.title.isEmpty  ? "새로운 메모" : memo.title
         configuration.secondaryAttributedText = memo.subtitle
         configuration.textProperties.numberOfLines = 1
         configuration.secondaryTextProperties.numberOfLines = 1
@@ -127,36 +155,5 @@ extension MemoListViewController: UITableViewDelegate {
             popOver.permittedArrowDirections = []
         }
         present(activityViewController, animated: true)
-    }
-}
-
-// MARK: - MemoDataManagerListDelegate
-
-extension MemoListViewController {
-    var selectedCellIndex: IndexPath? {
-        return tableView.indexPathForSelectedRow
-    }
-    
-    func setupRowSelection() {
-        tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: false, scrollPosition: .top)
-    }
-    
-    func addNewCell() {
-        tableView.reloadData()
-        tableView.selectRow(at: IndexPath(row: 0, section: 0), animated: true, scrollPosition: .top)
-    }
-    
-    func deleteCell(at indexPath: IndexPath) {
-        tableView.deleteRows(at: [indexPath], with: .none)
-    }
-    
-    func selectNextCell(at indexPath: IndexPath) {
-        tableView.allowsSelectionDuringEditing = true
-        tableView.selectRow(at: indexPath, animated: false, scrollPosition: .top)
-    }
-    
-    func updateCell(at indexPath: IndexPath) {
-        tableView.reloadRows(at: [indexPath], with: .none)
-        tableView.selectRow(at: indexPath, animated: true, scrollPosition: .none)
     }
 }

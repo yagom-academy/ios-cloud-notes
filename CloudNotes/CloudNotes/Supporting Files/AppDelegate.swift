dee6695ab20ee1b05a5bceed814097d509a42a53
JeongTaek Han <smart8612@gmail.com>
2022-02-24 15:50:29 +0900

    feat: SwiftyDropBox 의존성 추가 #8

--- CloudNotes/CloudNotes/Supporting Files/AppDelegate.swift
+++ CloudNotes/CloudNotes/Supporting Files/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import SwiftyDropbox
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +8,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        DropboxClientsManager.setupWithAppKey("ppezytas3cbnotq")
         return true
     }
     
